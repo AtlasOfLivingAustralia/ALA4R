552010696efb82dff9905a26a40dff20f45354e8
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-01-05 02:43:56 +0000

    finished doco so that the project builds -- doco still needs additional info

--- R/ala_species_info.R
+++ R/ala_species_info.R
@@ -1,10 +1,11 @@
-ala_species_info <-
-function(taxon) {
-taxon = ' 3red  kangaroo\n'
-taxon = clean_string(taxon) #clean up the taxon name
-out = fromJSON(file=paste("http://bie.ala.org.au/ws/search.json?q=",taxon,sep="")) #download the data
+#function to grab all data for a taxon of interest
+
+ala_species_info <- function(taxon) {
+	taxon = ' 3red  kangaroo\n'
+	taxon = clean_string(taxon) #clean up the taxon name
+	out = fromJSON(file=paste("http://bie.ala.org.au/ws/search.json?q=",taxon,sep="")) #download the data
 
 
-
+	return(out)
 
 }
