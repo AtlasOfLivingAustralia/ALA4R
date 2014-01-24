478960b130bcea69459c732bdf98549c920c12bb
Ben Raymond <ben@theraymonds.org>
2014-01-24 16:26:34 +1100

    added ala_fields

---
+++ R/ala_fields.R
@@ -0,0 +1,12 @@
+## Retrieve a list of all fields. For occurrence searching, http://biocache.ala.org.au/ws/index/fields. For searching searching taxon, datasets, layers, collections metadata, http://bie.ala.org.au/admin/indexFields
+
+ala_fields=function(fields_type="general") {
+    fields_type=tolower(fields_type)
+    match.arg(fields_type,c("general","occurrence"))
+    base_url="http://bie.ala.org.au/admin/indexFields"
+    if (identical(fields_type,"occurrence")) {
+        base_url="http://biocache.ala.org.au/ws/index/fields"
+    }
+    x=GET(url=base_url,user_agent(ala_user_agent()))
+    rbind.fill(lapply(content(x),as.data.frame)) ## convert each element of content(x)[[1]] into data frame, then combine
+}
