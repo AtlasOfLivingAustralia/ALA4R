fd9d617d0e23247a7dd51715a3e55e23a52448cf
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-01-27 12:07:01 +1000

    ensuring roxygen works for writing files

--- R/zzz.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(lib, pkg)  {
-	pkg.info <- utils::packageDescription('ALA4R') 
-	packageStartupMessage(paste("ALA4R ", pkg.info[["Version"]], " (", pkg.info["Date"], ")", sep=""))
-	return(invisible(0))
-}
