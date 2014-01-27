5c6a681c716fba8c9813db9c9c7a62689837f4c5
Ben Raymond <ben@theraymonds.org>
2014-01-27 12:25:39 +1100

    removed tests.R from R directory

--- R/tests.R
+++
@@ -1,6 +0,0 @@
-x=ala_autocomplete(taxon="macropus")
-
-my.names=list("Grevillea humilis","Grevillea humilis subsp. maritima")
-x=ala_bulklookup(my.names)
-
-x=ala_specieslist("macropus",wkt="POLYGON((140 -37,151 -37,151 -26,140.1310 -26,140 -37))")
