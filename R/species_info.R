15dbb7cfd6772c9dcd56173521192dacaf84478f
Ben Raymond <ben@theraymonds.org>
2014-01-28 13:51:42 +1100

    renamed and now using cached_get

--- R/species_info.R
+++ R/species_info.R
@@ -14,24 +14,16 @@
 #' @examples
 #' 
 #' 	#find information ALA holds on red kangaroo
-#' 	ala_species_info("red kangaroo")
+#' 	species_info("red kangaroo")
 #' 
-#' @export ala_species_info
-ala_species_info <- function(taxon) {
+#' @export species_info
+species_info <- function(taxon) {
 	taxon = clean_string(taxon) #clean up the taxon name
         base_url="http://bie.ala.org.au/ws/search.json"
         this_url=parse_url(base_url)
         this_url$query=list(q=taxon)
         this_url=build_url(this_url)
-        if (identical(ala_config()$caching,"off")) {
-            ## if we are not caching, get this directly without saving to file at all
-            x=GET(url=this_url,user_agent(ala_config()$user_agent))
-            x=content(x)
-        } else {
-            ## use caching
-            thisfile=download_to_file(this_url)
-            x=fromJSON(file=thisfile)
-        }
+        x=cached_get(url=this_url,type="json")
         x=x[[1]]
         ## reformat results to data frame
         x$results=rbind.fill(lapply(x$results,as.data.frame)) ## convert each element of results into data frame, then combine
