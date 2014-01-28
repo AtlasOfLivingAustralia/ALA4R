b06373b361ba701deec3463b61b3ce88db87c3f1
Ben Raymond <ben@theraymonds.org>
2014-01-28 15:14:42 +1100

    version bump and fixed typo

--- R/cached_get.R
+++ R/cached_get.R
@@ -5,7 +5,7 @@
 # @param url string: the url of the page to retrieve
 # @param type string: the expected content type. Either "text" (default), "json", or "filename"
 # @return for type=="text" the content is returned as text. For type=="json", the content is parsed using fromJSON. For "filename", the name of the stored file is returned.
-# @details Depending on the value of ala_config()$caching, the page is either retrieved from the cache or from the url, and stored in the cache if appropriate. The user-agent string is set according to ala_config()$user_agent. The returned response (if not from cached file) is also passed to check_http_status().
+# @details Depending on the value of ala_config()$caching, the page is either retrieved from the cache or from the url, and stored in the cache if appropriate. The user-agent string is set according to ala_config()$user_agent. The returned response (if not from cached file) is also passed to check_status_code().
 # @author Ben Raymond \email{ben@@theraymonds.org}, Jeremy VanDerWal \email{jjvanderwal@@gmail.com}
 # @examples
 #
@@ -19,7 +19,7 @@ cached_get=function(url,type="text",...) {
     if (identical(ala_config()$caching,"off") && !identical(type,"filename")) {
         ## if we are not caching, get this directly without saving to file at all
         x=GET(url=url,user_agent(ala_config()$user_agent))
-        check_http_status(x)
+        check_status_code(x)
         if (identical(type,"json")) {
             x=content(x,as="parsed")
         } else {
