2c2dea8a8173c819e213c0b3ab3be89f6fb700fb
Ben Raymond <ben@theraymonds.org>
2014-01-28 13:40:58 +1100

    renamed ala_bulklookup to bulklookup and used config base_url

--- R/bulklookup.R
+++ R/bulklookup.R
@@ -10,16 +10,16 @@
 #' @references \url{http://bie.ala.org.au/bie-service/}
 #' @examples
 #' 
-#' ala_bulklookup(list("Grevillea humilis","Grevillea humilis subsp. maritima"))
+#' bulklookup(list("Grevillea humilis","Grevillea humilis subsp. maritima"))
 #' 
-#' @export ala_bulklookup
-ala_bulklookup=function(taxa=list()) {
+#' @export bulklookup
+bulklookup=function(taxa=list()) {
     if (is.character(taxa)) {
         ## single name provided as a string, we were expecting a list of names
         taxa=list(taxa)
     }
     taxa = lapply(taxa,clean_string) ## clean up the taxon name
-    base_url="http://bie.ala.org.au/ws/species/bulklookup.json"
+    base_url=paste(ala_config()$base_url_bie,"species/bulklookup.json",sep="")
     x=POST(url=base_url,body=toJSON(taxa),user_agent(ala_config()$user_agent)) ## no caching on POST operations yet
     rbind.fill(lapply(content(x)[[1]],as.data.frame)) ## convert each element of content(x)[[1]] into data frame, then combine
 }
