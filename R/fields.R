393bb36aa234e1a50768357f14a4be205fa6e381
Ben Raymond <ben@theraymonds.org>
2014-01-28 15:04:47 +1100

    merged ala_fields into fields

--- R/fields.R
+++ R/fields.R
@@ -1,38 +1,58 @@
-#' List of fields of the environmental and contextual layers
+#' Retrieve a list of all fields
 #' 
-#' Used to provide a list of all fields associated with the environmental and contextual layers provided through ALA.
+#' Retrieves a list of field names that can be used with the data retrieval
+#' functions
 #' 
-#' @aliases field_info
-#' @param field_id field id; the field id value from the field() command
-#' @return A data frame of results
-#' @author Jeremy VanDerWal \email{jjvanderwal@@gmail.com}, Ben Raymond \email{ben@@theraymonds.org}
-#' @references \url{http://spatial.ala.org.au/ws/}
+#' 
+#' @param fields_type text: either "general" (for searching taxa, datasets,
+#' layers, and collections metadata), "occurrence" (for searching species
+#' occurrence records), or "layers" (a list of all fields associated with the environmental and contextual layers)
+#' @return A data frame containing the field names and various attributes
+#' @author Ben Raymond \email{ben@@theraymonds.org}, Jeremy VanDerWal
+#' \email{jjvanderwal@@gmail.com}
+#' @references For "occurrence",
+#' \url{http://biocache.ala.org.au/ws/index/fields}.  For "general",
+#' \url{http://bie.ala.org.au/ws/admin/indexFields}. For "layers", \url{http://spatial.ala.org.au/ws/field}
 #' @examples
 #' 
-#' fields()
+#' fields("general")
 #' field_info('cl22')
 #' 
 #' @export fields field_info
-fields = function() {
-    base_url = 'http://spatial.ala.org.au/ws/fields' #define the base url
-	out = GET(url=base_url,user_agent(ala_config()$user_agent)) #download all data
-	out = content(out) #keep only the content
-	do.call('rbind.fill',lapply(out,as.data.frame)) #bind the data as a dataframe
+
+fields=function(fields_type="general") {
+    fields_type=tolower(fields_type)
+    match.arg(fields_type,c("general","occurrence","layers"))
+    switch(fields_type,
+           "general"={base_url=paste(ala_config()$base_url_bie,"admin/indexFields",sep="")},
+           "occurrence"={base_url=paste(ala_config()$base_url_biocache,"index/fields",sep="")},
+           "layers"={base_url=paste(ala_config()$base_url_spatial,"fields",sep="")}
+           )
+
+    x=cached_get(base_url,type="json")
+    x=rbind.fill(lapply(x,as.data.frame)) ## convert each element of content(x)[[1]] into data frame, then combine
+        
+    ## convert factors to strings
+    for (col in 1:ncol(x)) {
+        if (identical(class(x[,col]),"factor")) {
+            x[,col]=as.character(x[,col])
+        }
+    }
+    x
 }
 
+
 field_info = function(field_id) {
-    base_url = 'http://spatial.ala.org.au/ws/field' #define the base url
-	out = GET(url=paste(base_url,field_id,sep='/'),user_agent(ala_config()$user_agent)) #download all data
-	if (out$headers$status != "200") { #check returned status 
-		stop('please check list of valid field ids using fields(). If your field id is correct, the ALA service may be down; please try again later.') 
-	}
-	if (substr(field_id,1,2) == 'cl') {
-		out = content(out)$objects #keep only the content
-		do.call('rbind.fill',lapply(out,as.data.frame)) #bind the data as a dataframe
-	} else if (substr(field_id,1,2) == 'el') {
-		out = content(out) #keep only the content
-		out = as.data.frame(rbind(out)) #bind the data as a dataframe	
-		rownames(out) = NULL #reset the row names
-		out
-	}
+    base_url = paste(ala_config()$base_url_spatial,"field",sep="")
+    out = cached_get(url=paste(base_url,field_id,sep='/'),type="json")
+    ## the http status code is checked as part of cached_get
+    ## however, we might wish to issue a warning for empty responses
+    if (substr(field_id,1,2) == 'cl') {
+        out = out$objects #keep only the content
+        do.call('rbind.fill',lapply(out,as.data.frame)) #bind the data as a dataframe
+    } else if (substr(field_id,1,2) == 'el') {
+        out = as.data.frame(rbind(out)) #bind the data as a dataframe	
+        rownames(out) = NULL #reset the row names
+        out
+    }
 }	
