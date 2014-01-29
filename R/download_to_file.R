f6989aba8b06a5e8a11e64ae482100cd60c78010
Ben Raymond <ben@theraymonds.org>
2014-01-29 23:06:59 +1100

    outfile can be specified

--- R/download_to_file.R
+++ R/download_to_file.R
@@ -1,11 +1,13 @@
-download_to_file=function(url,verbose=ala_config()$verbose,...) {
+download_to_file=function(url,outfile=NULL,verbose=ala_config()$verbose,...) {
     ## download from a URL using RCurl to a file
     ## we do this directly using RCurl to file, rather than reading into R memory and then dumping to file
-    outfile=ala_cache_filename(url)
+    if (is.null(outfile)) {
+        outfile=ala_cache_filename(url)
+    }
     
     ## are we using cached results?
     if ((ala_config()$caching %in% c("off","refresh")) || (! file.exists(outfile))) {
-        if (verbose) { cat(sprintf("  ALA4R: caching %s to file %s\n",url,outfile)) }
+        if (verbose && (ala_config()$caching != "off")) { cat(sprintf("  ALA4R: caching %s to file %s\n",url,outfile)) }
         ## either we are not using caching, or we want to refresh the cache, or the file doesn't exist in the cache
         f = CFILE(outfile, mode="w")
         h=basicHeaderGatherer()
