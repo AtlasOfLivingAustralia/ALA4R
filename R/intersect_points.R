adf9e2de25324b602914f87cddac8834265ed9b3
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-01-29 09:54:05 +0000

    bullet proofing of intersect_points()

--- R/intersect_points.R
+++ R/intersect_points.R
@@ -12,36 +12,41 @@
 #' #single point with multiple fields
 #' fields = c('cl22','cl23')
 #' pnts = c(-29,132.999)
-#' intersect(pnts,fields)
+#' intersect_points(pnts,fields)
 #' 
 #' #multiple points with multiple fields
 #' fields = c('cl22','cl23')
 #' pnts = data.frame(lat=seq(-29,-19,0.02),lon=132.769)
-#' intersect(pnts,fields)
+#' intersect_points(pnts,fields)
 #'
 #' @export
+
+####STILL to ADD
+# - functionality to provide points as a spatial points format from sp package
+#
+####
 
 intersect_points = function(pnts,fids) {
 	base_url=ala_config()$base_url_spatial #get the base url
 	bulk = FALSE #set the default to not bulk
 
 	###check and format the points
-	if (class(pnts) %in% c('data.frame','matrix')) { #convert to a vector if a data.frame or matrix
+	if (class(pnts) %in% c('data.frame','matrix')) { #convert to a vector if a data.frame or matrix and setup the string for the url
 		if (dim(pnts)[2] !=2) stop('data.frame or matrix of points must have 2 columns ordered lat,lon') #check the dimensions
-		if (nrow(pnts)==1) {
-			pnts_str = paste(pnts[1,],collapse='/',sep='')
-		} else {
+		if (nrow(pnts)==1) { #this is for a single coordinate pair
+			pnts_str = paste(pnts[1,],collapse='/',sep='') #setup the points str for the url
+		} else { #this is for the bulk intersect process where there is more than 1 coordinate pairs
 			if (nrow(pnts)>1001) stop('number of locations checked must be less than 1000') #ensure maximum limit is not breached
-			pnts_str = paste(paste(pnts[,1],pnts[,2],sep=','),collapse=',',sep='')
+			pnts_str = paste(paste(pnts[,1],pnts[,2],sep=','),collapse=',',sep='') #setup the points str for the url
 			bulk = TRUE #this is a bulk set of points
 		}
-	} else {
+	} else { #format the vector as a string for the url
 		if (length(pnts) %% 2 == 1) stop('vector of points must be paired locations... the length of this vector must be even') #check that the length of this is even 
-		if (length(pnts) == 2) {
-			pnts_str = paste(pnts,collapse='/',sep='')
-		} else {
+		if (length(pnts) == 2) { #this is for the single coordinate pair
+			pnts_str = paste(pnts,collapse='/',sep='') #setup the points str for the url
+		} else {  #this is for the bulk intersect process where there is more than 1 coordinate pairs
 			if (nrow(pnts)>2001) stop('number of locations checked must be less than 1000') #ensure maximum limit is not breached
-			pnts_str = paste(pnts,collapse=',',sep='')
+			pnts_str = paste(pnts,collapse=',',sep='') #setup the points str for the url
 			bulk = TRUE #this is a bulk set of points
 		}
 	}
@@ -64,14 +69,17 @@ intersect_points = function(pnts,fids) {
 		}
 		###not using caching because each query to this system will provide unique id & unique URL
 		tmpfile = tempfile() #define a temporary file
-		download.file(data_url$downloadUrl,tmpfile,mode='wb')
+		download.file(data_url$downloadUrl,tmpfile,mode='wb') #download the file
 		out = read.csv(unz(tmpfile,'sample.csv'),as.is=TRUE) #read in the csv data from the zip file
-		out
+		checks = NULL; for (ii in colnames(out)) { if (all(is.na(out[,ii]))) checks = c(checks,ii) } #identify bad field IDs
+		if (!is.null(checks)) warning(paste(paste(checks,collapse=', '),'are invalid field ids')) #warn user of bad field ids
+		return(out)
 	} else { #get results if just a single location
 		url_str = paste(base_url,'intersect/',fids_str,'/',pnts_str,sep='') #define the url string
 		out = cached_get(url_str,type="json") #get the data
-		as.data.frame(do.call('rbind',out)) #define the output
+		if (length(out)==0) stop('all field ids provided were invalid') #nothing returned if no valid IDs provided
+		out = do.call('rbind.fill',lapply(out,as.data.frame,stringsAsFactors=FALSE)) #define the output
+		checks = setdiff(fids,out$field); if (length(checks)>0) warning(paste(paste(checks,collapse=', '),'are invalid field ids')) #warn user of bad field ids
+		return(out)
 	}
-	
 }
-
