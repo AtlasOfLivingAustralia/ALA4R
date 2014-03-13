5732816d33fc52126a9133a52914201409e14be5
Ben Raymond <ben@theraymonds.org>
2014-03-13 19:01:26 +1100

    propagated on_server_error etc args

--- R/cached_get.R
+++ R/cached_get.R
@@ -5,6 +5,7 @@
 # @param url string: the url of the page to retrieve
 # @param type string: the expected content type. Either "text" (default), "json", or "filename" (this caches the content directly to a file and returns the filename without attempting to read it in)
 # @param caching string: caching behaviour, by default from ala_config()$caching
+# @param on_redirect, on_server_error, on_client_error function: passed to check_status_code()
 # @return for type=="text" the content is returned as text. For type=="json", the content is parsed using fromJSON. For "filename", the name of the stored file is returned.
 # @details Depending on the value of caching, the page is either retrieved from the cache or from the url, and stored in the cache if appropriate. The user-agent string is set according to ala_config()$user_agent. The returned response (if not from cached file) is also passed to check_status_code().
 # @author Atlas of Living Australia \email{support@@ala.org.au}
@@ -16,7 +17,7 @@
 
 ## TODO: change to using jsonlite for conversions, which generally gives more appropriate R data structures
 
-cached_get=function(url,type="text",caching=ala_config()$caching,verbose=ala_config()$verbose,...) {
+cached_get=function(url,type="text",caching=ala_config()$caching,verbose=ala_config()$verbose,on_redirect=NULL,on_client_error=NULL,on_server_error=NULL) {
     type=tolower(type)
     match.arg(type,c("text","json","filename"))
     
@@ -24,7 +25,7 @@ cached_get=function(url,type="text",caching=ala_config()$caching,verbose=ala_con
         ## if we are not caching, get this directly without saving to file at all
         if (verbose) { cat(sprintf("  ALA4R: GETting URL %s",url)) }
         x=GET(url=url,user_agent(ala_config()$user_agent))
-        check_status_code(x)
+        check_status_code(x,on_redirect=on_redirect,on_client_error=on_client_error,on_server_error=on_server_error)
         if (identical(type,"json")) {
             x=content(x,as="parsed")
         } else {
@@ -33,27 +34,31 @@ cached_get=function(url,type="text",caching=ala_config()$caching,verbose=ala_con
         x
     } else {
         ## use caching
-        thisfile=download_to_file(url)
-        if (type %in% c("json","text")) {
-            ## for json, read directly as string first, then pass this to fromJSON. This is compatible with either RJSON's or rjsonlite's version of fromJSON (only RJSON's version can take a filename directly)
-            if (!(file.info(thisfile)$size>0)) {
-                NULL
-            } else {
-                fid=file(thisfile, "rt")
-                out=readLines(fid,warn=FALSE)
-                close(fid)
-                if (identical(type,"json")) {
-                    fromJSON(out)
-                } else {
-                    out
-                }
-            }
-        } else if (identical(type,"filename")) {
-            thisfile
+        thisfile=download_to_file(url,on_redirect=on_redirect,on_client_error=on_client_error,on_server_error=on_server_error)
+        if (!file.exists(thisfile)) {
+            ## file does not exist
+            NULL
         } else {
-            ## should not be here! did we add an allowed type to the arguments without adding handler code down here?
-            stop(sprintf("unrecognized type %s in cached_post",type))
+            if (type %in% c("json","text")) {
+                ## for json, read directly as string first, then pass this to fromJSON. This is compatible with either RJSON's or rjsonlite's version of fromJSON (only RJSON's version can take a filename directly)
+                if (!(file.info(thisfile)$size>0)) {
+                    NULL
+                } else {
+                    fid=file(thisfile, "rt")
+                    out=readLines(fid,warn=FALSE)
+                    close(fid)
+                    if (identical(type,"json")) {
+                        fromJSON(out)
+                    } else {
+                        out
+                    }
+                }
+            } else if (identical(type,"filename")) {
+                thisfile
+            } else {
+                ## should not be here! did we add an allowed type to the arguments without adding handler code down here?
+                stop(sprintf("unrecognized type %s in cached_post",type))
+            }
         }
-        
     }
 }
--- R/download_to_file.R
+++ R/download_to_file.R
@@ -1,10 +1,16 @@
-download_to_file=function(url,outfile=NULL,verbose=ala_config()$verbose,...) {
+download_to_file=function(url,outfile=NULL,verbose=ala_config()$verbose,on_redirect=NULL,on_client_error=NULL,on_server_error=NULL,...) {
     ## download from a URL using RCurl to a file
     ## we do this directly using RCurl to file, rather than reading into R memory and then dumping to file
     if (is.null(outfile)) {
         outfile=ala_cache_filename(url)
     }
-    
+
+    ## first check for zero-size cached files
+    if (file.exists(outfile) && !(file.info(outfile)$size>0)) {
+        ## file exists but is zero sized
+        unlink(outfile)
+    }
+
     ## are we using cached results?
     if ((ala_config()$caching %in% c("off","refresh")) || (! file.exists(outfile))) {
         if (verbose && (ala_config()$caching != "off")) { cat(sprintf("  ALA4R: caching %s to file %s\n",url,outfile)) }
@@ -18,7 +24,10 @@ download_to_file=function(url,outfile=NULL,verbose=ala_config()$verbose,...) {
         if ((substr(h$value()[["status"]],1,1)=="5") || (substr(h$value()[["status"]],1,1)=="4")) {
             unlink(outfile)
         }
-        check_status_code(h$value()[["status"]])
+        ## check status code of response. Note that we execute the on_redirect etc functions, but we don't capture the output
+        ## might wish to implement this differently?
+        check_status_code(h$value()[["status"]],on_redirect=on_redirect,on_client_error=on_client_error,on_server_error=on_server_error)
+        
     } else {
         if (verbose) { cat(sprintf("  ALA4R: using cached file %s for %s\n",outfile,url)) }
     }
