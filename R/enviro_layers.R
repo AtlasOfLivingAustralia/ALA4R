d32aee4fcee91998f8962b72fa159b5d517c0d5d
Tasilee <leebelbin@gmail.com>
2014-03-14 13:58:23 +1100

    Lee's doc updates

--- R/enviro_layers.R
+++ R/enviro_layers.R
@@ -1,19 +1,28 @@
-#' List of environmental and contextual layers
+#' Provides a list of all environmental and contextual layers in ALA
 #' 
-#' Used to provide a list of all environmental and contextual layers
-#' provided through ALA.
-#' 
-#' @param type a selection of "all", "grids" or "shapes" associated with all possible layers, only environmental grids or contextual shapefiles
-#' @return A data frame of results
 #' @author Atlas of Living Australia \email{support@@ala.org.au}
-#' @references \url{http://api.ala.org.au/}
+#' @references \url{http://api.ala.org.au/} \url{http://spatial.ala.org.au/layers}
+#'
+#' @param type a selection of "all", "grids" or "shapes" associated with all possible layers, 
+#' only environmental grids or contextual shapefiles
+#' @return A data frame of results
+#' \item{name} \item{id} \item{type} \item{path} \item{description} \item{source}
+#' \item{displayname} \item{enabled} \item{uid} \item{metadatapath} \item{classification1}
+#' \item{classification2} \item{notes} \item{source_link} \item{licence_link} \item{licence_notes} 
+#' \item{maxlatitude} \item{minlatitude} \item{minlongitude} \item{maxlongitude}
+#' \item{pid} \item{shape} \item{path_orig} \item{environmentalvalueunits} \item{scale} 
+#' \item{environmentalvaluemax} \item{environmentalvaluemin} \item{lookuptablepath}
+#' \item{citation_date} \item{datalang} \item{licence_level} \item{mddataset}
+#' \item{mdhrlv} \item{respparty_role} \item{keywords} \item{domain}
+#' 
 #' @examples
 #' 
 #' enviro_layers(type="all")
 #' enviro_layers(type="grids")
 #' enviro_layers(type="shapes")
 #' 
-#' @export enviro_layers
+#' @export enviro_layers'
+#' 
 enviro_layers = function(type="all") {
     type=tolower(type)
     match.arg(type,c("all","grids","shapes"))
