d616ea46cadc294eeb2b12f44e4fc9d5780afa0f
Ben Raymond <ben@theraymonds.org>
2014-04-04 09:42:35 +1100

    added caching parameter

--- R/download_to_file.R
+++ R/download_to_file.R
@@ -1,4 +1,4 @@
-download_to_file=function(url,outfile=NULL,verbose=ala_config()$verbose,on_redirect=NULL,on_client_error=NULL,on_server_error=NULL,...) {
+download_to_file=function(url,outfile=NULL,caching=ala_config()$caching,verbose=ala_config()$verbose,on_redirect=NULL,on_client_error=NULL,on_server_error=NULL,...) {
     assert_that(is.string(url))
     ## download from a URL using RCurl to a file
     ## we do this directly using RCurl to file, rather than reading into R memory and then dumping to file
@@ -16,8 +16,8 @@ download_to_file=function(url,outfile=NULL,verbose=ala_config()$verbose,on_redir
     }
 
     ## are we using cached results?
-    if ((ala_config()$caching %in% c("off","refresh")) || (! file.exists(outfile))) {
-        if (verbose && (ala_config()$caching != "off")) { cat(sprintf("  ALA4R: caching %s to file %s\n",url,outfile)) }
+    if ((caching %in% c("off","refresh")) || (! file.exists(outfile))) {
+        if (verbose && (caching != "off")) { cat(sprintf("  ALA4R: caching %s to file %s\n",url,outfile)) }
         ## either we are not using caching, or we want to refresh the cache, or the file doesn't exist in the cache
         f = CFILE(outfile, mode="w")
         h=basicHeaderGatherer()
