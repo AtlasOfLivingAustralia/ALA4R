34133dc0103119cc362a96f09a63ccca2d543beb
Ben Raymond <ben@theraymonds.org>
2014-04-05 14:44:29 +1100

    using new occurrences service. needs work

--- R/occurrences.R
+++ R/occurrences.R
@@ -1,34 +1,38 @@
-#' Retrieve point occurrence records via the "Get occurrences as gzipped CSV" web service
+#' Get occurrence data
+#' 
+#' Retrieve ALA occurrence data via the "occurrence download" web service
 #' 
 #' @author Atlas of Living Australia \email{support@@ala.org.au}
 #' @references \url{http://api.ala.org.au/} 
-#' \url{https://docs.google.com/spreadsheet/ccc?key=0AjNtzhUIIHeNdHhtcFVSM09qZ3c3N3ItUnBBc09TbHc#gid=0}
+#' \url{https://docs.google.com/spreadsheet/ccc?key=0AjNtzhUIIHeNdHhtcFVSM09qZ3c3N3ItUnBBc09TbHc}
 #' \url{http://www.geoapi.org/3.0/javadoc/org/opengis/referencing/doc-files/WKT.html}
 #' 
 #' @param taxon string: taxonomic query, e.g. "macropus rufus"
 #' @param wkt string: a WKT (well-known text) string providing a spatial polygon within
 #' which to search, e.g. "POLYGON((140 -37,151 -37,151 -26,140.1310 -26,140 -37))"
-#' @param page_size integer: maximum number of records to return (may not be honoured by
-#' the ALA server). Default=NA, meaning that the server default value (currently 10) will be used.
 #' @param fields string vector: a vector of field names to return. Note that the columns of the returned data frame 
 #' are not guaranteed to retain the ordering of the field names given here. 
 #' See ALA4R funtion ala_fields("occurrence") for valid field names.
+#' @param download_reason_id integer: a reason code for the download. See ala_reasons() for valid values. The download_reason_id can be passed directly to this function, or alternatively set using ala_config(download_reason_id=...)
 #' 
 #' @return Data frame
 #' 
-#' TODO: Better error message for unfound taxon.
-#' 
 #' @examples
 #' \dontrun{ 
-#' x=occurrences(taxon="macropus",fields=c("longitude","latitude","common_name","taxon_name","el807"),page_size=1000)
+#' x=occurrences(taxon="macropus",fields=c("longitude","latitude","common_name","taxon_name","el807"))
+#' x=occurrences(taxon="data_resource_uid:dr356")
 #' 
-#' y=occurrences(taxon="alaba vibex",fields=c("latitude","longitude"),page_size=20)
-#' qv: http://biocache.ala.org.au/ws/occurrences/index/download?reasonTypeId=10&q=Alaba%20vibex&pageSize=20&fields=latitude,longitude&qa=none
+#' y=occurrences(taxon="alaba vibex",fields=c("latitude","longitude"))
+#' qv: http://biocache.ala.org.au/ws/occurrences/index/download?reasonTypeId=10&q=Alaba%20vibex&fields=latitude,longitude&qa=none
 #' }
 #' @export occurrences
+
+## note: this is a very sketchy first implementation of the new download API
+## currently using the TEST SERVER
+
 ## TODO: support extra params fq, startindex, etc (see API page)
 ## fq can query any field from http://biocache.ala.org.au/ws/index/fields, i.e. ala_fields("occurrence")
-
+## TODO: better error message for unfound taxon.
 
 
 ##q *  String	
@@ -76,80 +80,93 @@
 ##qa	String	
 ##A CSV list of record issues to include in the download. See assertions for possible values to include. When no value is supplied all the record issues are included. To include NO record issues set this value to none.
 
-occurrences=function(taxon="",wkt="",page_size=NA,fields=c()) {
-  ## check input parms are sensible
-  assert_that(is.string(taxon))
-  assert_that(is.string(wkt))
-  if (!is.na(page_size)) {
-    assert_that(is.count(page_size))
-  }
-  assert_that(is.character(fields))
-  
-  taxon = clean_string(taxon) ## clean up the taxon name
-  base_url=paste(ala_config()$base_url_biocache,"occurrences/download",sep="")
-  
-  this_query=list()
-  ## have we specified a taxon?
-  if (str_length(taxon)>0) {
-    this_query$q=taxon
-  }
-  ## wkt string
-  if (str_length(wkt)>0) {
-    if (! check_wkt(wkt)) {
-      stop("invalid WKT string ",wkt)
+occurrences=function(taxon="",wkt="",fields=c(),download_reason_id=ala_config()$download_reason_id) {
+    ## check input parms are sensible
+    reason_ok=!is.na(download_reason_id)
+    if (reason_ok) {
+        valid_reasons=ala_reasons()
+        reason_ok=download_reason_id %in% valid_reasons$id
     }
-    this_query$wkt=wkt
-  }
-  if (length(this_query)==0) {
-    ## not a valid request!
-    stop("invalid request: need at least taxon or wkt to be specified")
-  }
-  ## page_size
-  if (!is.na(page_size)) {
-    this_query$pageSize=page_size
-  }
-  
-  ## may wish to specify file=data or data.csv to make sure the file within the zip is consistently named
-  
-  if (length(fields)>0) {
-    ## user has specified some fields
-    valid_fields=ala_fields(fields_type="occurrence")
-    unknown=setdiff(fields,valid_fields$name)
-    if (length(unknown)>0) {
-      stop("invalid fields requested: ", str_c(unknown,collapse=", "))
+    if (! reason_ok) {
+        stop("download_reason_id must be a valid reason_id. See ala_reasons(). Set this value directly here or through ala_config(download_reason_id=...)")
     }
-    this_query$fl=str_c(fields,collapse=",")
-  }
+    
+    
+    assert_that(is.string(taxon))
+    assert_that(is.string(wkt))
   
-  this_url=parse_url(base_url)
-  this_url$query=this_query
+    #taxon = clean_string(taxon) ## clean up the taxon name # no - because this can be an indexed query like field1:value1
+    #base_url=paste(ala_config()$base_url_biocache,"occurrences/index/download",sep="")
+    base_url="http://biocache-test.ala.org.au/ws/occurrences/index/download" ## until new changes get propagated to live server
+    
   
-  ## these downloads can potentially be large, so we want to download directly to file and then read the file
-  thisfile=cached_get(url=build_url(this_url),type="filename")
-  if (!(file.info(thisfile)$size>0)) {
-    ## empty file
-    x=NULL
-  } else {
-    x=read.table(unz(thisfile,filename="data.csv"),sep=",",header=TRUE,comment.char="",as.is=TRUE)
-    ## this is very slow. I'd like to use e.g. data.table with the line below, but the embedded quotes are double-quote escaped rather than backslash-escaped
-    ##xdt=fread("../../temp/data.csv",sep=",",stringsAsFactors=FALSE,header=TRUE,verbose=ala_config()$verbose)
-    ## use read.table for now, until those issues are resolved
+    this_query=list()
+    ## have we specified a taxon?
+    if (str_length(taxon)>0) {
+        this_query$q=taxon
+    }
+    ## wkt string
+    if (str_length(wkt)>0) {
+        if (! check_wkt(wkt)) {
+            stop("invalid WKT string ",wkt)
+        }
+        this_query$wkt=wkt
+    }
+    if (length(this_query)==0) {
+        ## not a valid request!
+        stop("invalid request: need at least taxon or wkt to be specified")
+    }
+  
+    ## may wish to specify file=data or data.csv to make sure the file within the zip is consistently named
     
-    ##Taxon.identification.issue has embedded quotes, e.g. "[""noIssue""]"
+    if (length(fields)>0) {
+        assert_that(is.character(fields))
+        ## user has specified some fields
+        valid_fields=ala_fields(fields_type="occurrence")
+        unknown=setdiff(fields,valid_fields$name)
+        if (length(unknown)>0) {
+            stop("invalid fields requested: ", str_c(unknown,collapse=", "))
+        }
+        this_query$fields=str_c(fields,collapse=",")
+    }
+    this_query$reasonTypeId=download_reason_id
+    this_query$esc="\\" ## force backslash-escaping of quotes rather than double-quote escaping
     
-    ## also read the citation info
-    ##xc=read.table(unz(thisfile,"citation.csv"),sep=",",header=TRUE,comment.char="",as.is=TRUE)
-    ## nope, that doesn't work because there are line breaks WITHIN text field values
-    ##fid=unz(thisfile,filename="citation.csv",open="rt")
-    ##suppressWarnings(xc<-scan(fid,what="character",sep=NULL,quiet=TRUE,allowEscapes=TRUE)) ## suppress warnings here, otherwise we get warnings about newlines being embedded within quoted string. Would be better to be more selective about the warnings we are suppressing, or use a method that does not throw such a warning
-    ## nope, that gets confused by newline chars
-    fid=unz(thisfile,filename="citation.csv",open="rt")
-    xc=readChar(fid,nchars=1e6)        
-    close(fid)
-    
-    ## read the header line separately
-    xc_hdr=read.table(unz(thisfile,filename="citation.csv"),sep=",",header=FALSE,comment.char="",as.is=TRUE,nrows=1)
-    xc=as.data.frame(xc)
-  }
-  x
+    this_url=parse_url(base_url)
+    this_url$query=this_query
+  
+    ## these downloads can potentially be large, so we want to download directly to file and then read the file
+    thisfile=cached_get(url=build_url(this_url),type="filename")
+    if (!(file.info(thisfile)$size>0)) {
+        ## empty file
+        x=NULL
+    } else {
+        x=read.table(unz(thisfile,filename="data.csv"),sep=",",header=TRUE,comment.char="",as.is=TRUE)
+        ## this is very slow. I'd like to use e.g. data.table with the line below, but the embedded quotes are double-quote escaped rather than backslash-escaped
+        ##xdt=fread("../../temp/data.csv",sep=",",stringsAsFactors=FALSE,header=TRUE,verbose=ala_config()$verbose)
+        ## use read.table for now, until those issues are resolved
+        
+        ##Taxon.identification.issue has embedded quotes, e.g. "[""noIssue""]"
+        
+        ## also read the citation info
+        ##xc=read.table(unz(thisfile,"citation.csv"),sep=",",header=TRUE,comment.char="",as.is=TRUE)
+        ## nope, that doesn't work because there are line breaks WITHIN text field values
+        ##fid=unz(thisfile,filename="citation.csv",open="rt")
+        ##suppressWarnings(xc<-scan(fid,what="character",sep=NULL,quiet=TRUE,allowEscapes=TRUE)) ## suppress warnings here, otherwise we get warnings about newlines being embedded within quoted string. Would be better to be more selective about the warnings we are suppressing, or use a method that does not throw such a warning
+        ## nope, that gets confused by newline chars
+        ## note that citation.csv won't exist if there was no data in the download: need to check that it exists or wrap this in e.g. tryCatch
+        fid=unz(thisfile,filename="citation.csv",open="rt")
+        xc=readChar(fid,nchars=1e6)        
+        close(fid)
+
+        #fid=unz(thisfile,filename="citation.csv",open="rt")
+        #suppressWarnings(xc<-scan(fid,what="character",sep=",",quiet=TRUE,allowEscapes=TRUE))
+        # embedded quotes 
+        
+        ## read the header line separately
+        xc_hdr=read.table(unz(thisfile,filename="citation.csv"),sep=",",header=FALSE,comment.char="",as.is=TRUE,nrows=1)
+        xc=as.data.frame(xc)
+        x=list(data=x,meta=xc)
+    }
+    x
 }
