f6fbc7dde2213c4a286117de3029dfbac6adddb1
Tasilee <leebelbin@gmail.com>
2014-04-15 09:21:28 +1000

    Basic update of desscription and references

--- R/ALA4R.R
+++ R/ALA4R.R
@@ -1,6 +1,10 @@
 #' \pkg{ALA4R}
 #'
-#' This project enables the R community to access data and tools hosted at the Atlas of Living Australia (ALA: http://www.ala.org.au). The goal is to enable outputs (e.g. observations of species) to be queried and output in standard formats for R. ALA4R is based around use of the extensive web services provided by the Atlas; see References.
+#' This project enables the R community to access data and tools hosted by the 
+#' Atlas of Living Australia (ALA: http://www.ala.org.au). The goal of the project is 
+#' to enable basic species and related information to be queried and output in 
+#' standard formats for R. ALA4R is based around use of the extensive web services provided 
+#' by the Atlas; see References below for documentation of these web services.
 #' 
 #' @name ALA4R
 #' @author Atlas of Living Australia \email{support@@ala.org.au}, Ben Raymond, Jeremy VanDerWal, Lee Belbin
@@ -10,7 +14,7 @@
 #' The following web service reference pages are therefore added for completeness. 
 #' @references \url{http://bie.ala.org.au/ws}
 #' @references \url{http://biocache.la.org.au/ws}
-#' @references \url{http://spatial.ala.org.au/ws}
+#' @references \url{http://spatial.ala.org.au/layers-service/}
 #' 
 #' @import stringr httr plyr digest RCurl jsonlite assertthat
 NULL
