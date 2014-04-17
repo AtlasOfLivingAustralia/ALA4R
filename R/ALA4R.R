0f8e9f688899932a5e6579ce5437b115158bf785
Ben Raymond <ben@theraymonds.org>
2014-04-17 17:38:34 +1000

    documentation updated

--- R/ALA4R.R
+++ R/ALA4R.R
@@ -11,10 +11,12 @@
 #' @docType package
 #' @references \url{http://api.ala.org.au/ws}
 #' Currently, not all services of the following pages had been moved to api.ala.org.au
-#' The following web service reference pages are therefore added for completeness. 
-#' @references \url{http://bie.ala.org.au/ws}
-#' @references \url{http://biocache.la.org.au/ws}
-#' @references \url{http://spatial.ala.org.au/layers-service/}
+#' The following web service reference pages are therefore added for completeness:
+#' \itemize{
+#' \item \url{http://bie.ala.org.au/ws}
+#' \item \url{http://biocache.la.org.au/ws}
+#' \item \url{http://spatial.ala.org.au/layers-service/}
+#' }
 #' 
 #' @import stringr httr plyr digest RCurl jsonlite assertthat
 NULL
