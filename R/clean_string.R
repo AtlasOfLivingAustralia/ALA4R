db5d71bb608305c305bb469479366cabf7eac789
Ben Raymond <ben@theraymonds.org>
2014-04-24 20:15:30 +1000

    added dots to allowed characters

--- R/clean_string.R
+++ R/clean_string.R
@@ -1,5 +1,5 @@
 clean_string <- function(x) {
-	x = gsub("[^[:alpha:] ]", "", x) #remove anything but alpha characters
+	x = gsub("[^[:alpha:]\\. ]", "", x) #remove anything but alpha characters
 	x = str_trim(x) ## remove leading and trailing whitespaces
 	x = gsub('\\s+',' ',x) ## replace multiple whitespaces with single
 	x
