59603f47c92baa46aacd0571fc7f1b2b6d8804c1
Ben Raymond <ben@theraymonds.org>
2014-05-23 20:15:51 +1000

    doco tidyup

--- R/search_fulltext.R
+++ R/search_fulltext.R
@@ -1,70 +1,24 @@
 #' Full text search
 #' 
-#' Performs a search across all objects, and selects the closest matches
+#' Performs a search across all objects, and selects the closest matches. Generally, the user will provide the search term via the \code{query} parameter, with optional filtering via \code{fq}.
 #'
 #' @author Atlas of Living Australia \email{support@@ala.org.au}
 #' @references \url{http://api.ala.org.au/}
 #'  
-#' @param taxon string: (required) a character string for the taxon of interest
+#' @param query string: the search term
 #' @param fq string: (optional) character string or vector of strings, specifying filters to be applied to the 
 #' original query. These are of the form "INDEXEDFIELD:VALUE" e.g. "kingdom:Fungi". 
-#' See ala_fields("general") for all the fields that are queryable. 
+#' See \code{ala_fields("general")} for all the fields that are queryable. 
 #' NOTE that fq matches are case-sensitive, but sometimes the entries in the fields are 
 #' not consistent in terms of case (e.g. kingdom names "Fungi" and "Plantae" but "ANIMALIA"). 
 #' fq matches are ANDed by default (e.g. c("field1:abc","field2:def") will match records that have 
-#' field1 value "abc" and field2 value "def"). To obtain OR behaviour, use the form c("field1:abc 
-#' OR field2:def")
+#' field1 value "abc" and field2 value "def"). To obtain OR behaviour, use the form c("field1:abc OR field2:def")
 #' @param start numeric: (optional) (positive integer) start offset for the results
 #' @param page_size numeric: (optional) (positive integer) maximum number of records to return. Defaults to the server-side value - currently 10
 #' @param sort_by string: (optional) field to sort on
 #' @param sort_dir string: (optional) sort direction, either "asc" or "desc"
-#' 
-#' @return a named list with the components "meta" (search metadata), "facets" (search facet results), and "data" (the search results, in the form of a dataframe).
-# The data component is a dataframe with columns:
-# \itemize{
-# \item{guid}  
-# \item{name}  
-# \item{idxtype}
-# \item{score} 
-# \item{parentGuid}
-# \item{commonName}  
-# \item{nameComplete}
-# \item{commonNameSingle}  
-# \item{hasChildren} 
-# \item{rank} 
-# \item{rankId} 
-# \item{rawRank} 
-# \item{conservationStatus} 
-# \item{conservationStatusAUS}
-# \item{isAustralian}
-# \item{highlight}
-# \item{image} 
-# \item{thumbnail} 
-# \item{left} 
-# \item{right} 
-# \item{kingdom}
-# \item{phylum} 
-# \item{class} 
-# \item{order} 
-# \item{class} 
-# \item{order} 
-# \item{family} 
-# \item{genus} 
-# \item{author}
-# \item{linkIdentifier} 
-# \item{occCount} 
-# \item{imageSource} 
-# \item{imageCount} 
-# \item{isEcluded}
-# \item{imageUrl} 
-# \item{largeImageUrl} 
-# \item{smallImageUrl} 
-# \item{thumbnailUrl} 
-# \item{imageMetadataUrl} 
-# \item{acceptedConceptName} 
-# \item{synonomyRelationship} 
-# \item{synonomyDescription}
-# }
+#' @seealso \code{\link{ala_fields}}
+#' @return a named list with the components "meta" (search metadata), "facets" (search facet results), and "data" (the search results, in the form of a dataframe). The contents (column names) of the data frame will vary depending on the details of the search and the results.
 #' 
 #' @examples
 #'  # find information ALA holds on red kangaroo
@@ -76,11 +30,13 @@
 #'  search_fulltext("oenanthe",sort_by="kingdom",fq="rank:genus")
 #' 
 #' @export
-search_fulltext <- function(taxon,fq,start,page_size,sort_by,sort_dir) {
-        ##taxon = clean_string(taxon) #clean up the taxon name ## don't think it makes sense to do this for a fulltext search
+search_fulltext <- function(query,fq,start,page_size,sort_by,sort_dir) {
         base_url="http://bie.ala.org.au/ws/search.json"
         this_url=parse_url(base_url)
-        this_query=list(q=taxon)
+        this_query=list()
+        if (!missing(query)) {
+            this_query$q=query
+        }
         if (!missing(fq)) {
             assert_that(is.character(fq))
             ## can have multiple fq parameters, need to specify in url as fq=a:b&fq=c:d&fq=...
