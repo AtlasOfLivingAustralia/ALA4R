31a0245f248d51e011cd1d8dfb837b238979fd8d
Ben Raymond <ben@theraymonds.org>
2014-05-24 12:09:26 +1000

    code and documentation tidyup

--- R/search_layers.R
+++ R/search_layers.R
@@ -1,24 +1,12 @@
-#' Provides a list of all environmental and contextual layers in ALA
+#' Search for environmental and contextual data layers
 #' 
 #' @author Atlas of Living Australia \email{support@@ala.org.au}
 #' @references \url{http://api.ala.org.au/} \url{http://spatial.ala.org.au/layers}
 #'
-#' @param type string: a selection of "all", "grids" or "shapes" associated with all possible layers, 
-#' only environmental grids or contextual shapefiles
-#' @param query text string: optional search term against layer metadata. 
-#' Only layers that include this term in their metadata will be returned.
+#' @param type string: either "all" (all possible layers; default), "grids" (gridded environmental layers), or "shapes" (contextual shapefile layers)
+#' @param query text string: optional search term against layer metadata. Only layers that include this term in their metadata will be returned.
 #' 
-#' @return A data frame of results
-#' \itemize{
-#' \item{name} \item{id} \item{type} \item{path} \item{description} \item{source}
-#' \item{displayname} \item{enabled} \item{uid} \item{metadatapath} \item{classification1}
-#' \item{classification2} \item{notes} \item{source_link} \item{licence_link} \item{licence_notes} 
-#' \item{maxlatitude} \item{minlatitude} \item{minlongitude} \item{maxlongitude}
-#' \item{pid} \item{shape} \item{path_orig} \item{environmentalvalueunits} \item{scale} 
-#' \item{environmentalvaluemax} \item{environmentalvaluemin} \item{lookuptablepath}
-#' \item{citation_date} \item{datalang} \item{licence_level} \item{mddataset}
-#' \item{mdhrlv} \item{respparty_role} \item{keywords} \item{domain}
-#' }
+#' @return A data frame of results. The contents (column names) of the data frame will vary depending on the details of the search and the results.
 #' 
 #' @examples
 #' \dontrun{
@@ -28,28 +16,18 @@
 #' str(l)
 #' }
 #' @export
-#' 
-search_layers = function(type="all",query=NULL) {
+search_layers = function(type="all",query) {
     assert_that(is.string(type))
-    if (!is.null(query)) {
+    if (!missing(query)) {
         assert_that(is.string(query))
     }
     type=match.arg(tolower(type),c("all","grids","shapes"))
-    base_url = 'http://spatial.ala.org.au/ws/layers' #define the base url
-	if (type == 'all') { 
-            out = cached_get(url=base_url,type="json") #download all data
-	} else if (type == 'grids') {
-            out = cached_get(url=paste(base_url,type,sep='/'),type="json") #download only grids
-	} else if (type == 'shapes') {
-            out = cached_get(url=paste(base_url,type,sep='/'),type="json") #download only shapefile info
-	} else {
-            stop('type must be either all, grids or shape') #incorrect type so stop
-	}
-    #if NOT using jsonlite
-    #    out=do.call('rbind.fill',lapply(out,as.data.frame)) #bind the data as a dataframe
-    #    ## note that this gives strange format details (e.g. some dataframe elements are lists)
-    #}
-    if (!is.null(query)) {
+    base_url = 'http://spatial.ala.org.au/ws/layers' ## define the base url
+    if (type %in% c("grids","shapes")) {
+        base_url=paste(base_url,type,sep='/')
+    }
+    out = cached_get(url=base_url,type="json") ## download all data
+    if (!missing(query)) {
         out=out[grepl(query,out$name,ignore.case=TRUE) | grepl(query,out$description,ignore.case=TRUE),]
     }
     out
