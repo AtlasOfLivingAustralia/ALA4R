b96b678f7b489b9a96856c695c4871b2b1c83b9c
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-05-24 12:02:25 +0000

    interim commit on occurrence summary work

--- R/occurrences.R
+++ R/occurrences.R
@@ -188,6 +188,21 @@ occurrences=function(taxon,wkt,fq,fields,extra,qa,download_reason_id=ala_config(
     x
 }
 
+#@export
+summary.occurrences = function(x) {
+	#number of species, number of observations, proportion unique, proportion with assertion issues,
+	cat('number of species:',length(unique(x$data$ScientificName)),'\n')
+	cat('number of observation records:',nrow(x$data),'\n')
+	ass = check_assertions(x) #need to get existing assertions in occur dataset
+	if (nrow(ass)>0) {
+		cat('assertions included:',print(ass$occur.colnames,collapse=', '),'\n')
+		# roi.fatal = roi.missing = roi.warning = roi.error = NULL
+		# for (coi in ass$occur.colnames) {
+			# if (ass$fatal
+		# }
+	} else { cat('no asserting issues\n')
+}
+
 ## private function to change full field names to their id values (e.g. "Radiation - lowest period (Bio22)" to id "el871")
 #
 # replaced by fields_description_to_id in ala_fields
