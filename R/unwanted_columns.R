39b2a8d97e19cafaf87ba35dac6d666ed88749a1
Ben Raymond <ben@theraymonds.org>
2014-05-24 18:54:53 +1000

    added unwanted columns for layers type

--- R/unwanted_columns.R
+++ R/unwanted_columns.R
@@ -1,9 +1,9 @@
 ## define column names that we will remove from the results because we don't think they will be useful in the ALA4R context
 unwanted_columns=function(type) {
-    type=match.arg(tolower(type),c("general"))
-    if (type=="general") {
-        c("rankId","left","right","idxType","nameComplete","hasChildren","highlight","linkIdentifier","isExcluded")
-    } else {
-        c("")
-    }
+    type=match.arg(tolower(type),c("general","layers"))
+    switch(type,
+           "general"=c("rankId","left","right","idxType","nameComplete","hasChildren","highlight","linkIdentifier","isExcluded"),
+           "layers"=c("pid","path","path_orig","enabled","uid","licence_level","lookuptablepath","mdhrlv"),
+           c("")
+           )
 }
