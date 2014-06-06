37c5d31d7c51ba6dd9f909c8e61973e2a355097e
Ben Raymond <ben@theraymonds.org>
2014-06-06 11:15:09 +1000

    minor code tidy

--- R/species_by_site.R
+++ R/species_by_site.R
@@ -31,62 +31,61 @@
 #'
 #' @export
 species_by_site = function(taxon,wkt,gridsize=0.1,SPdata.frame=FALSE,verbose=ala_config()$verbose) {
-	###TODO data checks
-	###todo setup output structure and class
-	###todo api movingaveragesize is unnecessary... consider removing...
-	## check input parms are sensible
+    ##TODO data checks
+    ##todo setup output structure and class
+    ##todo api movingaveragesize is unnecessary... consider removing...
+    ## check input parms are sensible
     assert_that(is.string(taxon))
     assert_that(is.string(wkt))
     assert_that(is.logical(SPdata.frame))
 	
-    base_url="http://biocache.ala.org.au/ws/webportal/occurrences.gz"
-
     ## wkt string supplied and valid?
     if (str_length(wkt)>0) {
         if (! check_wkt(wkt)) {
             warning("WKT string appears to be invalid: ",wkt)
         }
-	}
-	
-	###setup the key query
-	base_url = ala_config()$base_url_alaspatial #get the base url
-	url_str = paste(base_url,'sitesbyspecies?speciesq=',taxon,'&qname=data',sep='') #setup the base url string 
-	url_str = paste(url_str,'&area=',wkt,sep='') #append the area info
-	url_str = paste(url_str,'&bs=',ala_config()$base_url_biocache,sep='') # append hte biocache URL string
-	url_str = paste(url_str,'&movingaveragesize=1',sep='') #append hte moving window average value
-	url_str = paste(url_str,'&gridsize=',gridsize,sep='') #append hte grid size
-	url_str = paste(url_str,'&sitesbyspecies=1',sep='') #define the type
-	
-	this_cache_file=ala_cache_filename(url_str) ## the file that will ultimately hold the results (even if we are not caching, it still gets saved to file)
-	if ((ala_config()$caching %in% c("off","refresh")) || (! file.exists(this_cache_file))) {
-		pid = cached_post(URLencode(url_str),'',caching='off') #should simply return a pid
-		if (pid=="") { stop("there has been an issue with this service. Please try again but if the issue persists, contact support@@ala.org.au") } #catch for these missing pid issues
-		status_url = paste('http://spatial.ala.org.au/alaspatial/ws/job?pid=',pid,sep='')
-		status=cached_get(URLencode(status_url),type="json",caching="off")#get the data url
-		while (status$state != "SUCCESSFUL") { if(verbose) { cat('.') } #keep checking the status until finished
-			status=cached_get(status_url,type="json",caching="off") #get the status
-			if (status$state=='FAILED') { stop(status$message) } #stop if there was an error
-			Sys.sleep(2)
-		}; cat('\n')
-		download_to_file(paste('http://spatial.ala.org.au/alaspatial/ws/download/',pid,sep=''),outfile=this_cache_file,binary_file=TRUE)
-	} else {
-		## we are using the existing cached file
-		if (verbose) { cat(sprintf("  ALA4R: using cached file %s\n",this_cache_file)) }
-	}
-	out = read.csv(unz(this_cache_file,'SitesBySpecies.csv'),as.is=TRUE,skip=4) #read in the csv data from the zip file; omit the first 4 header rows
+    }
+    
+    ##setup the key query
+    base_url = ala_config()$base_url_alaspatial #get the base url
+    url_str = paste(base_url,'sitesbyspecies?speciesq=',taxon,'&qname=data',sep='') #setup the base url string 
+    url_str = paste(url_str,'&area=',wkt,sep='') #append the area info
+    url_str = paste(url_str,'&bs=',ala_config()$base_url_biocache,sep='') # append hte biocache URL string
+    url_str = paste(url_str,'&movingaveragesize=1',sep='') #append hte moving window average value (1 = 1 cell, which means that no moving average applied)
+    url_str = paste(url_str,'&gridsize=',gridsize,sep='') #append hte grid size
+    url_str = paste(url_str,'&sitesbyspecies=1',sep='') #define the type
+    
+    this_cache_file=ala_cache_filename(url_str) ## the file that will ultimately hold the results (even if we are not caching, it still gets saved to file)
+    if ((ala_config()$caching %in% c("off","refresh")) || (! file.exists(this_cache_file))) {
+        pid = cached_post(URLencode(url_str),'',caching='off') #should simply return a pid
+        if (pid=="") { stop("there has been an issue with this service. Please try again but if the issue persists, contact support@@ala.org.au") } #catch for these missing pid issues
+        status_url = paste('http://spatial.ala.org.au/alaspatial/ws/job?pid=',pid,sep='')
+        status=cached_get(URLencode(status_url),type="json",caching="off")#get the data url
+        while (status$state != "SUCCESSFUL") {
+            if(verbose) { cat('.') } #keep checking the status until finished
+            status=cached_get(status_url,type="json",caching="off") #get the status
+            if (status$state=='FAILED') { stop(status$message) } #stop if there was an error
+            Sys.sleep(2)
+        }; cat('\n')
+        download_to_file(paste('http://spatial.ala.org.au/alaspatial/ws/download/',pid,sep=''),outfile=this_cache_file,binary_file=TRUE)
+    } else {
+        ## we are using the existing cached file
+        if (verbose) { cat(sprintf("  ALA4R: using cached file %s\n",this_cache_file)) }
+    }
+    out = read.csv(unz(this_cache_file,'SitesBySpecies.csv'),as.is=TRUE,skip=4) #read in the csv data from the zip file; omit the first 4 header rows
 
-	###deal with SpatialPointsDataFrame
-	if (SPdata.frame) { #if output is requested as a SpatialPointsDataFrame
-		if (is.element('sp', installed.packages()[,1])) { #if sp package is available
-			require(sp) #load the library		
-			## coerce to SpatialPointsDataFrame class
-			if (nrow(out)>0) {
-				out=SpatialPointsDataFrame(coords=out[,c("Longitude","Latitude")],proj4string=CRS("+proj=longlat +ellps=WGS84"),data=out)
-			}
-		} else {
-			warning('sp package needs to be installed; data output is output as a simple dataframe')
-		}
-	}
-	###return the output
-    return(out)
+    ##deal with SpatialPointsDataFrame
+    if (SPdata.frame) { #if output is requested as a SpatialPointsDataFrame
+        if (is.element('sp', installed.packages()[,1])) { #if sp package is available
+            require(sp) #load the library		
+            ## coerce to SpatialPointsDataFrame class
+            if (nrow(out)>0) {
+                out=SpatialPointsDataFrame(coords=out[,c("Longitude","Latitude")],proj4string=CRS("+proj=longlat +ellps=WGS84"),data=out)
+            }
+        } else {
+            warning('sp package needs to be installed; data output is output as a simple dataframe')
+        }
+    }
+    ##return the output
+    out
 }
