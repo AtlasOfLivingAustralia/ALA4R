8b54b17a674b9d04da8cbface91fc468781f8010
Ben Raymond <ben@theraymonds.org>
2014-06-09 14:50:20 +1000

    changed output format: some list elements are now data.frames

--- R/species_info.R
+++ R/species_info.R
@@ -6,7 +6,7 @@
 #' @param scientificname string: scientific name of the taxon of interest (species, genus, family etc) 
 #' @param guid string: The Life Science Identifier of the taxon of interest
 #' @param verbose logical: how much progress information to display; default is set by \code{ala_config()}
-#' @return species profile in the form of a named list
+#' @return species profile in the form of a named list, each element of which is generally a data frame
 
 #' @examples
 #' 
@@ -41,5 +41,14 @@ species_info=function(scientificname,guid,verbose=ala_config()$verbose) {
         guid=guid[[1]]
     }
     url=paste(ala_config()$base_url_bie,"species/",guid,".json",sep="")
-    cached_get(URLencode(url),type="json",verbose=verbose)
+    out=cached_get(URLencode(url),type="json",verbose=verbose)
+    ## rename a couple of things
+    names(out$classification)=str_replace_all(names(x$classification),"^clazz","class")
+    ## restructure any list children of out to be data.frames
+    for (k in 1:length(out)) {
+        if (is.list(out[[k]])) {
+            out[[k]]=as.data.frame(out[[k]])
+        }
+    }
+    out
 }
