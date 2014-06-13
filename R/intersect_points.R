c645051e71c0a3091cc3d247ffd96292900a85d3
Ben Raymond <ben@theraymonds.org>
2014-06-14 09:36:02 +1000

    added parm type checking; minor bugfix (nrows for vector pnts input); added name-to-id conversion on fids

--- R/intersect_points.R
+++ R/intersect_points.R
@@ -8,8 +8,8 @@
 #' number of environmental (el) and contextual (cl) layers but intersect_points could be used
 #' to sample background (non species-site) values of layers.
 #' 
-#' @param pnts numeric: vector of lat & lons or 2 column data.frame or matrix of lat,lons. NOTE: the number of locations must be less than 1000.
-#' @param fids string: ids of layers to be intersected. list of possible layers is available from ala_fields().
+#' @param pnts numeric: vector of latitude/longitude pairs, or a 2 column data.frame or matrix of lat,lons. NOTE: the number of locations must be less than 1000.
+#' @param fids string vector: ids of layers to be intersected. The list of possible layers is available from ala_fields("layers").
 #' @param SPdata.frame logical: should the output should be returned as a SpatialPointsDataFrame of the sp package?
 #' @param verbose logical: show additional progress information? [default is set by ala_config()]
 #' @return A SpatialPointsDataFrame containing the intersecting data information. Missing data or incorrectly identified field id values will result in NA data
@@ -34,80 +34,89 @@
 #' 
 #' }
 #' @export
-#'
+
+## undocumented: field ids in "fids" can be passed as full names (e.g. "Radiation - lowest period (Bio22)") rather than id ("el871"). I haven't documented this (yet) until it is implemented across all functions - BR
+
 intersect_points = function(pnts,fids,SPdata.frame=FALSE,verbose=ala_config()$verbose) {
-	base_url=ala_config()$base_url_spatial #get the base url
-	bulk = FALSE #set the default to not bulk
+    ## input parameter checking
+    assert_that(is.numeric(pnts))
+    assert_that(is.character(fids))
+    assert_that(is.flag(SPdata.frame))
+    assert_that(is.flag(verbose))
+    
+    base_url=ala_config()$base_url_spatial #get the base url
+    bulk = FALSE #set the default to not bulk
 
-	###check and format the points
-	if (class(pnts) %in% c('data.frame','matrix')) { #convert to a vector if a data.frame or matrix and setup the string for the url
-		if (dim(pnts)[2] !=2) stop('data.frame or matrix of points must have 2 columns ordered lat,lon') #check the dimensions
-		if (nrow(pnts)==1) { #this is for a single coordinate pair
-			pnts_str = paste(pnts[1,],collapse='/',sep='') #setup the points str for the url
-		} else { #this is for the bulk intersect process where there is more than 1 coordinate pairs
-			if (nrow(pnts)>1001) stop('number of locations checked must be less than 1000') #ensure maximum limit is not breached
-			pnts_str = paste(paste(pnts[,1],pnts[,2],sep=','),collapse=',',sep='') #setup the points str for the url
-			bulk = TRUE #this is a bulk set of points
-		}
-	} else { #format the vector as a string for the url
-		if (length(pnts) %% 2 == 1) stop('vector of points must be paired locations... the length of this vector must be even') #check that the length of this is even 
-		if (length(pnts) == 2) { #this is for the single coordinate pair
-			pnts_str = paste(pnts,collapse='/',sep='') #setup the points str for the url
-		} else {  #this is for the bulk intersect process where there is more than 1 coordinate pairs
-			if (nrow(pnts)>2001) stop('number of locations checked must be less than 1000') #ensure maximum limit is not breached
-			pnts_str = paste(pnts,collapse=',',sep='') #setup the points str for the url
-			bulk = TRUE #this is a bulk set of points
-		}
-	}
-	###format the fields string
-	if (length(fids)>1) {
-		fids_str = paste(fids,collapse=',',sep='')
-	} else {
-		fids_str = fids
-	}
-
-	###download the data
-	if (bulk) { #get the results if it is a bulk request
-		url_str = paste(base_url,'intersect/batch?fids=',fids_str,'&points=',pnts_str,sep='') #define the url string
-                url_str=URLencode(url_str) ## should not be needed, but do it anyway
-		this_cache_file=ala_cache_filename(url_str) ## the file that will ultimately hold the results (even if we are not caching, it still gets saved to file)
-		if ((ala_config()$caching %in% c("off","refresh")) || (! file.exists(this_cache_file))) {
-			## fetch the data from the server
-			## we use cached_get operations here even though we're not caching, just because it keeps the user-agent etc string consistent
-			#status_url=cached_get(url_str,type="json",caching="off")$statusUrl #submit the url and get the url of the status
-			status_url=jsonlite::fromJSON(paste(readLines(url_str,warn=FALSE),collapse = ""))$statusUrl ## using GET (in cached_get) for this can give 414 errors ("url too long") which do not seem to happen with readLines, so use that until we figure out why
-			data_url=cached_get(status_url,type="json",caching="off") #get the data url
-			while (data_url$status != 'finished') { #keep checking the status until finished
-				Sys.sleep(5)
-				data_url=cached_get(status_url,type="json",caching="off") #get the data url
-			}
-			download_to_file(data_url$downloadUrl,outfile=this_cache_file,binary_file=TRUE)
-		} else {
-			## we are using the existing cached file
-			if (verbose) { cat(sprintf("  ALA4R: using cached file %s\n",this_cache_file)) }
-		}
-		out = read.csv(unz(this_cache_file,'sample.csv'),as.is=TRUE) #read in the csv data from the zip file
-		checks = NULL; for (ii in colnames(out)) { if (all(is.na(out[,ii]))) checks = c(checks,ii) } #identify bad field IDs
-		if (!is.null(checks)) warning(paste(paste(checks,collapse=', '),'are invalid field ids')) #warn user of bad field ids
-	} else { #get results if just a single location
-		url_str = paste(base_url,'intersect/',fids_str,'/',pnts_str,sep='') #define the url string
-                url_str=URLencode(url_str) ## should not be needed, but do it anyway
-		out = cached_get(url_str,type="json") #get the data
-		if (length(out)==0) stop('all field ids provided were invalid') #nothing returned if no valid IDs provided
-		tt = t(out$value); colnames(tt) = out$field 
-		out = data.frame(latitude=pnts[1],longitude=pnts[2],tt) # define the output the same as the bulk output
-		#if NOT using jsonlite
-		#    out = do.call('rbind.fill',lapply(out,as.data.frame,stringsAsFactors=FALSE)) #define the output
-		#}
-		checks = setdiff(fids,colnames(out)[-c(1,2)]); if (length(checks)>0) warning(paste(paste(checks,collapse=', '),'are invalid field ids')) #warn user of bad field ids
-	}
-	###deal with SpatialPointsDataFrame
-	if (SPdata.frame) { #if output is requested as a SpatialPointsDataFrame
-            ## coerce to SpatialPointsDataFrame class
-            if (nrow(out)>0) {
-                out=SpatialPointsDataFrame(coords=out[,c("longitude","latitude")],proj4string=CRS("+proj=longlat +ellps=WGS84"),data=out)
+    ##check and format the points
+    if (class(pnts) %in% c('data.frame','matrix')) { #convert to a vector if a data.frame or matrix and setup the string for the url
+        if (dim(pnts)[2] !=2) stop('data.frame or matrix of points must have 2 columns ordered lat,lon') #check the dimensions
+        if (nrow(pnts)==1) { #this is for a single coordinate pair
+            pnts_str = paste(pnts[1,],collapse='/',sep='') #setup the points str for the url
+        } else { #this is for the bulk intersect process where there is more than 1 coordinate pairs
+            if (nrow(pnts)>1001) stop('number of locations checked must be less than 1000') #ensure maximum limit is not breached
+            pnts_str = paste(paste(pnts[,1],pnts[,2],sep=','),collapse=',',sep='') #setup the points str for the url
+            bulk = TRUE #this is a bulk set of points
+        }
+    } else { #format the vector as a string for the url
+        if (length(pnts) %% 2 == 1) stop('vector of points must be paired locations... the length of this vector must be even') #check that the length of this is even 
+        if (length(pnts) == 2) { #this is for the single coordinate pair
+            pnts_str = paste(pnts,collapse='/',sep='') #setup the points str for the url
+        } else {  #this is for the bulk intersect process where there is more than 1 coordinate pairs
+            if (length(pnts)>2001) stop('number of locations checked must be less than 1000') #ensure maximum limit is not breached
+            pnts_str = paste(pnts,collapse=',',sep='') #setup the points str for the url
+            bulk = TRUE #this is a bulk set of points
+        }
+    }
+    ##format the fields string
+    fids=fields_description_to_id(fields=fids,fields_type="layers") ## replace long names with ids
+    if (length(fids)>1) {
+        fids_str = paste(fids,collapse=',',sep='')
+    } else {
+        fids_str = fids
+    }
+    
+    ##download the data
+    if (bulk) { #get the results if it is a bulk request
+        url_str = paste(base_url,'intersect/batch?fids=',fids_str,'&points=',pnts_str,sep='') #define the url string
+        url_str=URLencode(url_str) ## should not be needed, but do it anyway
+        this_cache_file=ala_cache_filename(url_str) ## the file that will ultimately hold the results (even if we are not caching, it still gets saved to file)
+        if ((ala_config()$caching %in% c("off","refresh")) || (! file.exists(this_cache_file))) {
+            ## fetch the data from the server
+            ## we use cached_get operations here even though we're not caching, just because it keeps the user-agent etc string consistent
+                                        #status_url=cached_get(url_str,type="json",caching="off")$statusUrl #submit the url and get the url of the status
+            status_url=jsonlite::fromJSON(paste(readLines(url_str,warn=FALSE),collapse = ""))$statusUrl ## using GET (in cached_get) for this can give 414 errors ("url too long") which do not seem to happen with readLines, so use that until we figure out why
+            data_url=cached_get(status_url,type="json",caching="off") #get the data url
+            while (data_url$status != 'finished') { #keep checking the status until finished
+                Sys.sleep(5)
+                data_url=cached_get(status_url,type="json",caching="off") #get the data url
             }
-	}
-	###return the output
-    return(out)
+            download_to_file(data_url$downloadUrl,outfile=this_cache_file,binary_file=TRUE)
+        } else {
+            ## we are using the existing cached file
+            if (verbose) { cat(sprintf("  ALA4R: using cached file %s\n",this_cache_file)) }
+        }
+        out = read.csv(unz(this_cache_file,'sample.csv'),as.is=TRUE) #read in the csv data from the zip file
+        checks = NULL; for (ii in colnames(out)) { if (all(is.na(out[,ii]))) checks = c(checks,ii) } #identify bad field IDs
+        if (!is.null(checks)) warning(paste(paste(checks,collapse=', '),'are invalid field ids')) #warn user of bad field ids
+    } else { #get results if just a single location
+        url_str = paste(base_url,'intersect/',fids_str,'/',pnts_str,sep='') #define the url string
+        url_str=URLencode(url_str) ## should not be needed, but do it anyway
+        out = cached_get(url_str,type="json") #get the data
+        if (length(out)==0) stop('all field ids provided were invalid') #nothing returned if no valid IDs provided
+        tt = t(out$value); colnames(tt) = out$field 
+        out = data.frame(latitude=pnts[1],longitude=pnts[2],tt) # define the output the same as the bulk output
+                                        #if NOT using jsonlite
+                                        #    out = do.call('rbind.fill',lapply(out,as.data.frame,stringsAsFactors=FALSE)) #define the output
+                                        #}
+        checks = setdiff(fids,colnames(out)[-c(1,2)]); if (length(checks)>0) warning(paste(paste(checks,collapse=', '),'are invalid field ids')) #warn user of bad field ids
+    }
+    ##deal with SpatialPointsDataFrame
+    if (SPdata.frame) { #if output is requested as a SpatialPointsDataFrame
+        ## coerce to SpatialPointsDataFrame class
+        if (nrow(out)>0) {
+            out=SpatialPointsDataFrame(coords=out[,c("longitude","latitude")],proj4string=CRS("+proj=longlat +ellps=WGS84"),data=out)
+        }
+    }
+    ##return the output
+    out
 }
