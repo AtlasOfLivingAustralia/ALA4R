61ba3f04dc798bbb138c7cf3c4eb748a5558513b
Ben Raymond <ben@theraymonds.org>
2014-06-14 09:00:34 +1000

    clean_string moved to utilities_internal.R

--- R/clean_string.R
+++
@@ -1,6 +0,0 @@
-clean_string <- function(x) {
-	x = gsub("[^[:alpha:]\\. ]", "", x) #remove anything but alpha characters
-	x = str_trim(x) ## remove leading and trailing whitespaces
-	x = gsub('\\s+',' ',x) ## replace multiple whitespaces with single
-	x
-}
--- R/utilities_internal.R
+++ R/utilities_internal.R
@@ -18,3 +18,11 @@ convert_dt=function(x,test_numeric=TRUE) {
     }
     x
 }
+
+
+clean_string <- function(x) {
+	x = gsub("[^[:alpha:]\\. ]", "", x) #remove anything but alpha characters
+	x = str_trim(x) ## remove leading and trailing whitespaces
+	x = gsub('\\s+',' ',x) ## replace multiple whitespaces with single
+	x
+}
