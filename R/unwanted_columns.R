62e08b1eb2a3b6439abf7f34731317b3b6e6d5d0
Ben Raymond <ben@theraymonds.org>
2014-06-14 20:32:31 +1000

    updated layer columns

--- R/unwanted_columns.R
+++ R/unwanted_columns.R
@@ -2,8 +2,13 @@
 unwanted_columns=function(type) {
     type=match.arg(tolower(type),c("general","layers"))
     switch(type,
-           "general"=c("rankId","left","right","idxType","nameComplete","hasChildren","highlight","linkIdentifier","isExcluded"),
-           "layers"=c("pid","path","path_orig","enabled","uid","licence_level","lookuptablepath","mdhrlv"),
+           "general"=c("rankId","left","right","idxType","highlight","linkIdentifier","isExcluded","hasChildren"),
+             ## hasChildren seems always to be false, even for taxa that ought to have children (e.g. Macropus)
+           "layers"=c("pid","path","path_orig","path_1km","enabled","uid","licence_level","lookuptablepath","mdhrlv","mddatest","grid","shape"),
+             ## datalang appears to be all "eng" "Eng" "enu" "" or NA (2x"enu" records appear to be in English and from DEH/DEWHA)
+             ## grid is redundant: all env layers are grid==TRUE, all contextual layers are grid==NA
+             ## ditto for shape: all contextual are TRUE, all grid are NA
+             ## mddatest is an internal metadata testing date of some sort? 
            c("")
            )
 }
