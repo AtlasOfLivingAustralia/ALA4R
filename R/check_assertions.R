275b0772ecd6453f7936e8ac3329c3e820cca682
Ben Raymond <ben@theraymonds.org>
2014-06-20 18:19:17 +1000

    updated for new variable names

--- R/check_assertions.R
+++ R/check_assertions.R
@@ -9,25 +9,27 @@
 #' @return A dataframe of assertions column names, descriptions and categories/error codes.
 #'
 #' @examples
-#' \dontrun{
 #' #download species data with all possible assertions
 #' x=occurrences(taxon="golden bowerbird",download_reason_id=10,qa=ala_fields('assertions')$name)
 #' (asserts = check_assertions(x)) #data.frame of assertions, their description and column names
-#' }
 #' @export
 check_assertions = function(x) {
-	if (any(class(x)=='occurrences')) {
-		cois = colnames(x$data) #get the column names
-		ass = ala_fields('assertions') #get the assertions
-		ass$occur.colnames = NA
-		for(coi in cois) {
-			tt = c(grep(tocamel(coi),tocamel(ass$name)),grep(tocamel(coi),tocamel(ass$description))) #check if in name or description columns
-			if (length(tt)==0) {
-				next
-			} else {
-				ass$occur.colnames[tt[1]] = coi #place the colname
-			}
-		}
-		na.omit(ass)
-	} else { stop('check_assertions must have an object of class occurrences from e.g., occurrences() in the ALA4R package') }
+    if (any(class(x)=='occurrences')) {
+        cois = colnames(x$data) #get the column names
+        ass = ala_fields('assertions') #get the assertions
+        ass$occurColnames = NA
+        for(coi in cois) {
+            tt=c(grep(coi,ass$name),grep(coi,rename_variables(ass$description,type="assertions")))
+            ## old code for previous variable names
+            ## tt = c(grep(tocamel(coi),tocamel(ass$name)),grep(tocamel(coi),tocamel(ass$description))) #check if in name or description columns
+            if (length(tt)==0) {
+                next
+            } else {
+                ass$occurColnames[tt[1]] = coi #place the colname
+            }
+        }
+        na.omit(ass)
+    } else {
+        stop('check_assertions must have an object of class occurrences from e.g., occurrences() in the ALA4R package')
+    }
 }
