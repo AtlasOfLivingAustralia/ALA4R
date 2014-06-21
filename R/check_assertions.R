8b13c24a5545ab2c2d829ad319f845de5183ee92
Jeremy VanDerWal <jjvanderwal@gmail.com>
2014-06-21 07:17:08 +0000

    main update - return NULL if no assertions

--- R/check_assertions.R
+++ R/check_assertions.R
@@ -6,7 +6,7 @@
 #' 
 #' @param x list: an object returned from \code{\link{occurrences}}
 #' 
-#' @return A dataframe of assertions column names, descriptions and categories/error codes.
+#' @return A dataframe of assertions column names, descriptions and categories/error codes. If no assertions are in dataset, NULL is returned.
 #'
 #' @examples
 #' #download species data with all possible assertions
@@ -21,15 +21,14 @@ check_assertions = function(x) {
         temp_description=rename_variables(ass$description,type="assertions")
         for(coi in cois) {
             tt=which(coi==ass$name | coi==temp_description)
-            ## old code for previous variable names
-            ## tt = c(grep(tocamel(coi),tocamel(ass$name)),grep(tocamel(coi),tocamel(ass$description))) #check if in name or description columns
             if (length(tt)==0) {
                 next
             } else {
                 ass$occurColnames[tt[1]] = coi #place the colname
             }
         }
-        na.omit(ass)
+        ass = na.omit(ass)
+		if (nrow(ass)==0) { warning('no assertions in data'); return(NULL) } else { return(ass) }
     } else {
         stop('check_assertions must have an object of class occurrences from e.g., occurrences() in the ALA4R package')
     }
