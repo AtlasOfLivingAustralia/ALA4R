0ffaf66b0dc5a59e6923ecc7433ee1eb445fdc51
Tasilee <leebelbin@gmail.com>
2014-06-25 16:24:39 +1000

    Test and update doco include 2nd example

--- R/occurrences_plot.R
+++ R/occurrences_plot.R
@@ -1,14 +1,20 @@
-#' Quick plot of occurrence data
+#' Quick geographic plot of occurrence data
 #' 
-#' Generates a plot of occurrence data retrieved using \code{\link{occurrences}}.
+#' Generates a plot of occurrence data retrieved using \code{\link{occurrences}}.The plot uses
+#' an Australian basemap and colours the occurrence records dots according to parameters
 #' 
 #' @author Atlas of Living Australia \email{support@@ala.org.au}
+#' @references \url{http://api.ala.org.au/}
 #' 
 #' @param x list: a list object that has been downloaded using \code{\link{occurrences}}
 #' @param filename string: name of file to be created; defaults to RPlots.pdf
-#' @param qa string vector: list of record issues to be mapped; these can be assertion columnnames, or 'all' or 'none' or any combination of 'error', 'warning' or 'fatal'. Column or categories in your dataset can be viewed using \code{check_assertions}. 
-#' @param grouped logical: TRUE creates a single plot for all observations; FALSE plots individual maps for the taxon level defined.
-#' @param taxon_level string: taxonomic level at which to create maps; possible values are 'species', 'genus', 'family' or 'order'
+#' @param qa string vector: list of record issues to be mapped; these can be assertion columnnames, 
+#' or 'all' or 'none' or any combination of 'error', 'warning' or 'fatal'. Column or categories in 
+#' your dataset can be viewed using \code{check_assertions}. 
+#' @param grouped logical: TRUE creates a single plot for all observations; FALSE plots individual 
+#' maps for the taxon level defined.
+#' @param taxon_level string: taxonomic level at which to create maps; possible values are 'species', 
+#' 'genus', 'family' or 'order'
 #' @param \dots : other options passed to pdf()
 #' @return Generates a pdf that maps the distributions.
 #' 
@@ -17,6 +23,8 @@
 #' #download some observations
 #' x=occurrences(taxon="golden bowerbird",download_reason_id=10)
 #' occurrences_plot(x)
+#' x=occurrences(taxon="Cider Gum",download_reason_id=10)
+#' occurrences_plot(x,"alaPlot.pdf",qa="fatal",grouped=FALSE, taxon_level="species")
 #' }
 #' @export occurrences_plot
 occurrences_plot = function(x, filename='Rplots.pdf', qa=c('fatal','error'), grouped=FALSE, taxon_level='species',...) 
