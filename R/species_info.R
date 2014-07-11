23a5c02af2f3ead236644881231602de609b023b
Ben Raymond <ben@theraymonds.org>
2014-07-11 21:17:12 +1000

    changed return value on empty result to list() not NULL, caught problem with un-matched GUID inputs

--- R/species_info.R
+++ R/species_info.R
@@ -6,7 +6,7 @@
 #' @param scientificname string: scientific name of the taxon of interest (species, genus, family etc) 
 #' @param guid string: The Life Science Identifier of the taxon of interest
 #' @param verbose logical: show additional progress information? [default is set by ala_config()]
-#' @return species profile in the form of a named list, each element of which is generally a data frame
+#' @return A species profile in the form of a named list, each element of which is generally a data frame. An empty list is returned if no match is found for the supplied name or guid
 #' @seealso \code{\link{ala_config}}
 #' @examples
 #' 
@@ -38,12 +38,16 @@ species_info=function(scientificname,guid,verbose=ala_config()$verbose) {
     if (!missing(scientificname)) {
         guid=search_names(scientificname,vernacular=FALSE,guids_only=TRUE)
         if (length(guid)<1) {
-            return(NULL)
+            return(list())
         }
         guid=guid[[1]]
     }
     url=paste(ala_config()$base_url_bie,"species/",guid,".json",sep="")
     out=cached_get(URLencode(url),type="json",verbose=verbose)
+    if (is.null(out)) {
+        ## invalid guids will give NULL here, catch them now
+        return(list())
+    }
     ## restructure any list children of out to be data.frames
     for (k in 1:length(out)) {
         if (is.list(out[[k]])) {
