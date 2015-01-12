6943df18196c7cf738423ba37929604aef16aaa3
Ben Raymond <ben@theraymonds.org>
2015-01-12 15:57:43 +1100

    updated check_fq to handle negated queries and brackets

--- R/check_fq.R
+++ R/check_fq.R
@@ -5,8 +5,11 @@ check_fq=function(fq,type) {
     assert_that(is.string(type))
     type=match.arg(tolower(type),c("general","occurrence","layers"))
     temp=paste("",fq,collapse=" ") ## with leading space
+    temp=gsub("[\\(\\)]+"," ",temp) ## drop all brackets (we don't care about the logic, just the field names that are present) and replace with spaces
     field_names=str_split(temp,"\\s+") ## split on spaces
     field_names=unlist(str_extract_all(unlist(field_names),perl("^[^\\:]+(?=\\:)"))) ## extract "field" in field:value
+    field_names=gsub("^\\-","",field_names) ## remove leading "-" characters which can be used to negate fq queries
+    cat(str(field_names))
     valid_fields=ala_fields(type)
     invalid_fields=setdiff(field_names,valid_fields$name)
     if (length(invalid_fields)>0) {
