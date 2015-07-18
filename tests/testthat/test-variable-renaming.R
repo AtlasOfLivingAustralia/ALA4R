ab1f6448385868e021839b51a918a932e21b4daa
Ben Raymond <ben@theraymonds.org>
2015-07-18 20:03:16 +1000

    tests for variable renaming

---
+++ tests/testthat/test-variable-renaming.R
@@ -0,0 +1,18 @@
+context("Test that variables are renamed correctly")
+
+test_that("acronyms remain uppercase", {
+    expect_equal(rename_variables("IMCRA","assertions"),"iMCRA")
+    expect_equal(rename_variables("IMCRA","occurrence"),"IMCRA")
+})
+
+test_that("underscores are renamed to camelCase", {
+    expect_equal(rename_variables("this_that","occurrence"),"thisThat")
+})
+
+test_that("particular variables are renamed for occurrence data", {
+    temp=c("scientificName","matchedScientificName","recordID","xVersion","MatchTaxonConceptGUID","vernacularName","taxonRank","matchedsomething","processedsomething","parsedsomething")
+    temp2=rename_variables(temp,"occurrence")
+    expect_true(!any(temp==temp2))
+})
+
+          
