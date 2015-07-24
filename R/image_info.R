62ef7c19dcc98837cb674b49d51bc45e2df04374
Ben Raymond <ben@theraymonds.org>
2015-07-25 08:53:32 +1000

    handle invalid image IDs

--- R/image_info.R
+++ R/image_info.R
@@ -6,10 +6,11 @@
 #' 
 #' @param id character: IDs of images (e.g. as returned by \code{\link{occurrences}}  in the imageUrl column). Each ID will be of a format something like "84654e14-dc35-4486-9e7c-40eb2f8d3faa"
 #' @param verbose logical: show additional progress information? [default is set by ala_config()]
-#' @return A data.frame with one row per \code{id}
+#' @return A data.frame with one row per \code{id}, and at least the columns imageIdentifier and imageURL
 #' @seealso \code{\link{ala_config}} \code{\link{occurrences}}
 #' @examples
-#' image_info(c("84654e14-dc35-4486-9e7c-40eb2f8d3faa","b8344134-254d-4116-a98d-4a37e7362a4e"))
+#' image_info(c("84654e14-dc35-4486-9e7c-40eb2f8d3faa","39836d30-0761-473d-bac2-9ed9494fd37e","this-is-an-invalid-image-id"))
+#'
 #' @export image_info
 
 image_info=function(id,verbose=ala_config()$verbose) {
@@ -21,7 +22,8 @@ image_info=function(id,verbose=ala_config()$verbose) {
     non_empty=nchar(id)>0 & !is.na(id)
     ## grab each image info web page
     this_url=paste0("http://images.ala.org.au/image/details?imageId=",id[non_empty])
-    pages=sapply(this_url,function(z) paste0(cached_get(URLencode(z),type="text",verbose=verbose),collapse=" "))
+    ## we get a 500 error if we ask for a non-existent image ID, so catch these errors with the on_server_error parm
+    pages=sapply(this_url,function(z) paste0(cached_get(URLencode(z),type="text",verbose=verbose,on_server_error=function(z)NULL),collapse=" "))
     ## keep only the table from each, which has the actual image details
     if (packageVersion("stringr")<"1.0") {    
         pages=sapply(pages,function(z) { if (nrow(str_locate(z,"<table"))==1) str_extract(z,"<table.*</table>") else stop("image information page in unexpected format: please notify the ALA4R maintainers") })
@@ -29,19 +31,15 @@ image_info=function(id,verbose=ala_config()$verbose) {
         pages=sapply(pages,function(z) { if (nrow(str_locate(z,"<table"))==1) str_extract(z,regex("<table.*</table>",dotall=TRUE)) else stop("image information page in unexpected format: please notify the ALA4R maintainers") })
     }        
         
-    ##out=data.frame(id=id)
-    ## image properties look something like <td class="property-name">Image URL</td> <td class="property-value">http://images.ala.org.au/store/a/a/f/3/84654e14-dc35-4486-9e7c-40eb2f8d3faa/original </tr>
-    ##temp=extract_image_detail(pages,"Image URL")
-    ##out$imageUrl[non_empty]=temp[,1]
-
-    ##out=dlply(pages,function(z)
-    
-    extract_image_detail(pages,".*?")
+    out=extract_image_detail(pages,".*?")
+    ## this will have NA values for imageIdentifier where id was not valid (because the imageIdentifier value comes from the scraped HTML, which won't exist for invalid id). Replace with input ids
+    out$imageIdentifier=id
+    out
 }
 
 ## helper function to extract detail from the scraped html pages
 extract_image_detail=function(html,property_name_regex) {
-    regex_str=paste0("<td[^>]*>(",property_name_regex,")</td>\\s*<td[^>]*>([^>]*)</")
+    regex_str=paste0("<td[^>]*>(",property_name_regex,")</td>\\s*<td[^>]*>(.*?)</t[rd]")
     if (packageVersion("stringr")<"1.0") {
         out=str_match_all(html,ignore.case(regex_str))
         ## For stringr version < 1, elements will be empty character matrices if there are no matches
@@ -50,7 +48,13 @@ extract_image_detail=function(html,property_name_regex) {
         out=str_match_all(html,regex(regex_str,ignore_case=TRUE))
     }
     ## out is a list, where each element is a character matrix where nrows = number of matches to property_name_regex and ncols=3
-    out=ldply(out,function(z) { this=as.data.frame(t(str_trim(z[,3])),stringsAsFactors=FALSE); names(this)=z[,2]; this } )
-    names(out)=rename_variables(names(out),type="occurrence")
-    out
+    ##  if image ID was invalid, then nrows=1 and each entry is NA
+    ldply(out,function(z) {
+        if (nrow(z)==1 && is.na(z[,3])) {
+            data.frame(imageURL=NA)
+        } else {
+            this=as.data.frame(t(str_trim(z[,3])),stringsAsFactors=FALSE)
+            names(this)=rename_variables(z[,2],type="occurrence")
+            this
+        }} )
 }
--- tests/testthat/test-imageinfo.R
+++ tests/testthat/test-imageinfo.R
@@ -2,6 +2,8 @@ context("Test image_info-related functions")
 
 ala_config(caching="off")
 
+expected_property_names=sort(c("imageIdentifier","title","creator","dataResourceUID","filename","dimensionsWXH","fileSize","dateUploaded","uploadedBy","dateTakenCreated","mimeType","zoomLevels","linearScale","imageURL","MD5Hash","SHA1Hash","sizeOnDiskIncludingAllArtifacts","rights","rightsHolder","licence","harvestedAsOccurrenceRecord"))
+
 test_that("extract_image_detail matches html as expected", {
     expect_equal(extract_image_detail(c("<td>blah</td><td>thing</td>","thiswillnotmatch"),"blah")[1,1],"thing")
     expect_equal(extract_image_detail(c("<td>blah</td> <td>thing</td>","thiswillnotmatch"),"blah")[2,1],as.character(NA))
@@ -13,11 +15,21 @@ test_that("extract_image_detail matches html as expected", {
 test_that("image_info works as expected on known records", {
     known_image_info=image_info("84654e14-dc35-4486-9e7c-40eb2f8d3faa")
     expect_equal(nrow(known_image_info),1)
-    expect_equal(sort(names(known_image_info)),sort(c("imageIdentifier","title","creator","dataResourceUID","filename","dimensionsWXH","fileSize","dateUploaded","uploadedBy","dateTakenCreated","mimeType","zoomLevels","linearScale","imageURL","MD5Hash","SHA1Hash","sizeOnDiskIncludingAllArtifacts","rights","rightsHolder","licence","harvestedAsOccurrenceRecord")))
+    expect_equal(sort(names(known_image_info)),expected_property_names)
 
-    known_image_info=image_info(c("84654e14-dc35-4486-9e7c-40eb2f8d3faa","b8344134-254d-4116-a98d-4a37e7362a4e"))
+    known_image_info=image_info(c("84654e14-dc35-4486-9e7c-40eb2f8d3faa","39836d30-0761-473d-bac2-9ed9494fd37e"))
     expect_equal(nrow(known_image_info),2)
-    expect_equal(sort(names(known_image_info)),sort(c("imageIdentifier","title","creator","dataResourceUID","filename","dimensionsWXH","fileSize","dateUploaded","uploadedBy","dateTakenCreated","mimeType","zoomLevels","linearScale","imageURL","MD5Hash","SHA1Hash","sizeOnDiskIncludingAllArtifacts","rights","rightsHolder","licence","harvestedAsOccurrenceRecord")))    
+    expect_equal(sort(names(known_image_info)),expected_property_names)    
+})
+
+test_that("image_info works with un-matched records", {
+    mixed_image_info=image_info(c("84654e14-dc35-4486-9e7c-40eb2f8d3faa","this-is-an-invalid-image-id","39836d30-0761-473d-bac2-9ed9494fd37e","this-is-also-an-invalid-image-id"))
+    expect_equal(nrow(mixed_image_info),4)
+    expect_equal(sort(names(mixed_image_info)),expected_property_names)    
+})
+
+test_that("image_info handles embedded html in property value td block", {    
+    expect_equal(sort(names(image_info("b8344134-254d-4116-a98d-4a37e7362a4e"))),expected_property_names)
 })
 
     
