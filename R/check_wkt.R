1ea9a8aa9ac9e206aa38eea7425b21e9d083f20a
Ben Raymond <ben@theraymonds.org>
2016-03-30 19:59:36 +1100

    typo

--- R/check_wkt.R
+++ R/check_wkt.R
@@ -1,6 +1,6 @@
 ## simple function to check that a given WKT string is valid
 ## prior to v1.25 this was an ALA4R-specific (and not very comprehensive) function
-## as of v1.25 we rely on wellformed::lint
+## as of v1.25 we rely on wellknown::lint
 check_wkt=function(wkt,silent=TRUE) {
     assert_that(is.string(wkt)) ## leave this in place for consistency with older code
     wellknown::lint(wkt)
