9d2ced9eb8c01b42b221235bed97dbb148e08140
Ben Raymond <ben@theraymonds.org>
2016-07-30 21:30:55 +1000

    modified tests for updated download reasons

--- tests/testthat/test-occurrences.R
+++ tests/testthat/test-occurrences.R
@@ -3,9 +3,9 @@ context("Test occurrence-related functions")
 ## ala_reasons
 thischeck=function() {
     test_that("ala_reasons works as expected", {
-        expect_that(ala_reasons(),has_names(c("rkey","name","id")))
-        expect_that(nrow(ala_reasons()),equals(11))
-        expect_equal(sort(ala_reasons()$id),0:10)
+        expect_that(ala_reasons(),has_names(c("rkey","name","id","deprecated")))
+        expect_that(nrow(ala_reasons()),equals(13))
+        expect_equal(sort(ala_reasons()$id),0:12)
         expect_error(ala_reasons(TRUE)) ## this should throw and error because there is an unused argument
     })
 }
