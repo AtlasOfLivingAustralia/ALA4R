86e04517d0c53b9dc7cac024ba562c675ea329d6
Ben Raymond <ben@theraymonds.org>
2016-09-14 19:38:17 +1000

    code tidy, slightly better example

--- R/check_assertions.R
+++ R/check_assertions.R
@@ -12,31 +12,34 @@
 #'
 #' @examples
 #' #download species data with all possible assertions
-#' x=occurrences(taxon="golden bowerbird",download_reason_id=10,qa=ala_fields('assertions')$name)
-#' asserts = check_assertions(x) #data.frame of assertions, their description and column names
-#' asserts[0:86,5] # List out descriptions of all (current) assertions
+#' x <- occurrences(taxon="golden bowerbird",download_reason_id=10,qa=ala_fields("assertions")$name)
+#' asserts <- check_assertions(x) #data.frame of assertions, their description and column names
+#' asserts$description # List out descriptions of all (current) assertions
+#'
+#' tmp <- x$data[,names(x$data) %in% asserts$name] ## assertion columns from data
+#' which(colSums(tmp)>0) ## discard those not seen in the data
 #' 
 #' @export
-check_assertions = function(x) {
+check_assertions <- function(x) {
     if (! inherits(x,"occurrences")) {
-        stop('check_assertions must have an object of class occurrences from e.g., occurrences() in the ALA4R package')
+        stop("check_assertions must have an object of class occurrences from e.g., occurrences() in the ALA4R package")
     }
-    cois = colnames(x$data) #get the column names
-    ass = ala_fields('assertions',as_is=TRUE) #get the assertions
-    ass$occurColnames = NA
-    temp_description=rename_variables(ass$description,type="assertions")
+    cois <- colnames(x$data) #get the column names
+    ass <- ala_fields("assertions",as_is=TRUE) #get the assertions
+    ass$occurColnames <- NA
+    temp_description <- rename_variables(ass$description,type="assertions")
     for(coi in cois) {
-        tt=which(coi==ass$name | coi==temp_description)
+        tt <- which(coi==ass$name | coi==temp_description)
         if (length(tt)==0) {
             next
         } else {
-            ass$occurColnames[tt[1]] = coi #place the colname
+            ass$occurColnames[tt[1]] <- coi #place the colname
         }
     }
-    ass = na.omit(ass)
+    ass <- na.omit(ass)
     if (nrow(ass)==0) {
         if (ala_config()$warn_on_empty) {
-            warning('no assertions in data');
+            warning("no assertions in data")
         }
         NULL
     } else {
