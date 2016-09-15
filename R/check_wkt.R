d57cee9d69ac840a33056f8e91054d990c2f961d
Ben Raymond <ben@theraymonds.org>
2016-09-16 07:52:08 +1000

    drop unused 'silent' parameter from check_wkt

--- R/check_wkt.R
+++ R/check_wkt.R
@@ -1,7 +1,7 @@
 ## simple function to check that a given WKT string is valid
 ## prior to v1.25 this was an ALA4R-specific (and not very comprehensive) function
 ## as of v1.25 we rely on wellknown::lint
-check_wkt=function(wkt,silent=TRUE) {
+check_wkt <- function(wkt) {
     assert_that(is.string(wkt)) ## leave this in place for consistency with older code
     wellknown::lint(wkt)
 }
