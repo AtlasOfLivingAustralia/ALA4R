78d47eefc1b28e2514a9ce0eb5f5f7aa1a2f48d9
Ben Raymond <ben@theraymonds.org>
2016-10-01 09:38:26 +1000

    old-style expectation syntax to new

--- tests/testthat/test-species.R
+++ tests/testthat/test-species.R
@@ -6,15 +6,15 @@ context("Test species information functions")
 
 thischeck=function() {
     test_that("species_info generally works as expected", {
-        expect_that(species_info("Grevillea humilis subsp. maritima"),is_a("list"))
+        expect_is(species_info("Grevillea humilis subsp. maritima"),"list")
         expect_error(species_info("Grevillea humilis subsp. maritima",verbose="yes"))
         expect_equal(species_info("Grevillea humilis subsp. maritima"),species_info(factor("Grevillea humilis subsp. maritima")))
         expect_true(all(names(species_info("Grevillea humilis subsp. maritima")) %in% c("taxonConcept","taxonName","classification","identifiers","synonyms","commonNames","childConcepts","parentConcepts","sameAsConcepts","pestStatuses","conservationStatuses","simpleProperties","images","imageIdentifier","distributionImages","screenshotImages","extantStatuses","habitats","regionTypes","references","publicationReference","identificationKeys","specimenHolding","categories","isAustralian","linkIdentifier","extantStatuses")))
-        expect_warning(expect_that(species_info(guid="urn:lsid:biodiversity.org.au:apni.taxon:248651"),is_a("list")))
-        expect_that(species_info(guid="bilbobaggins"),is_a("list")) ## empty result should still be a list
-        expect_that(species_info("bilbobaggins"),is_a("list")) ## empty result should still be a list
+        expect_warning(expect_is(species_info(guid="urn:lsid:biodiversity.org.au:apni.taxon:248651"),"list"))
+        expect_is(species_info(guid="bilbobaggins"),"list") ## empty result should still be a list
+        expect_is(species_info("bilbobaggins"),"list") ## empty result should still be a list
         ## this one no longer matches anything with new taxonomy
-        ##expect_that(species_info(guid="ALA_Pterostylis_squamata")$classification,is_a("data.frame")) ## taxon with improper classification
+        ##expect_is(species_info(guid="ALA_Pterostylis_squamata")$classification,"data.frame") ## taxon with improper classification
         ##expect_equal(species_info(guid="ALA_Pterostylis_squamata")$classification[[1]],"ORCHIDACEAE") ## taxon with improper classification
     })
     test_that("text encoding works as expected",{
