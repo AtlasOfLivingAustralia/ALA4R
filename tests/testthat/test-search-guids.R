9693515b437bf54a00a017c947f37eefd4525b65
Ben Raymond <ben@theraymonds.org>
2017-05-04 22:08:38 +1000

    change test guid to valid one

--- tests/testthat/test-search-guids.R
+++ tests/testthat/test-search-guids.R
@@ -1,7 +1,9 @@
 thischeck=function() {
     test_that("search_guids can cope with factor inputs", {
         skip_on_cran()
-        expect_equal(search_guids(factor("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59")),search_guids("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59"))
+
+        urn:lsid:biodiversity.org.au:afd.name:292120
+        expect_equal(search_guids(factor("urn:lsid:biodiversity.org.au:afd.name:292120")),search_guids("urn:lsid:biodiversity.org.au:afd.name:292120"))
     })
 }
 check_caching(thischeck)
@@ -9,7 +11,7 @@ check_caching(thischeck)
 thischeck=function() {
     test_that("search_guids can cope with mixed recognized/unrecogized guids", {
         skip_on_cran()
-        expect_equal(is.na(search_guids(c("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59","http://id.biodiversity.org.au/node/apni/2890970","this_is_not_a_valid_guid"))$guid),c(FALSE,FALSE,TRUE))
+        expect_equal(is.na(search_guids(c("urn:lsid:biodiversity.org.au:afd.name:292120","http://id.biodiversity.org.au/node/apni/2890970","this_is_not_a_valid_guid"))$guid),c(FALSE,FALSE,TRUE))
     })
 }
 check_caching(thischeck)
@@ -29,17 +31,17 @@ check_caching(thischeck)
 thischeck=function() {
     test_that("search_guids returns occurrence counts when asked", {
         skip_on_cran()
-        expect_false(is.na(search_guids("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59",occurrence_count=TRUE)$occurrenceCount))        
-        expect_equal(is.na(search_guids(c("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59","isdulfsadh"),occurrence_count=TRUE)$occurrenceCount),c(FALSE,TRUE))
+        expect_false(is.na(search_guids("urn:lsid:biodiversity.org.au:afd.name:292120",occurrence_count=TRUE)$occurrenceCount))        
+        expect_equal(is.na(search_guids(c("urn:lsid:biodiversity.org.au:afd.name:292120","isdulfsadh"),occurrence_count=TRUE)$occurrenceCount),c(FALSE,TRUE))
         expect_true(is.na(search_guids(c("blahblah"),occurrence_count=TRUE)$occurrenceCount))
         expect_false(is.list(search_guids(c("blahblah"),occurrence_count=TRUE)$occurrenceCount))
         expect_false(is.list(search_guids(c("blahblah","jdfhsdjk"),occurrence_count=TRUE)$occurrenceCount))
-        expect_false(is.list(search_guids(c("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59","blahblah","jdfhsdjk"),occurrence_count=TRUE)$occurrenceCount))
-        expect_false(is.list(search_guids(c("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59","http://id.biodiversity.org.au/node/apni/2890970"),occurrence_count=TRUE)$occurrenceCount))
+        expect_false(is.list(search_guids(c("urn:lsid:biodiversity.org.au:afd.name:292120","blahblah","jdfhsdjk"),occurrence_count=TRUE)$occurrenceCount))
+        expect_false(is.list(search_guids(c("urn:lsid:biodiversity.org.au:afd.name:292120","http://id.biodiversity.org.au/node/apni/2890970"),occurrence_count=TRUE)$occurrenceCount))
         expect_false(is.list(search_guids(c("http://id.biodiversity.org.au/node/apni/2890970"),occurrence_count=TRUE)$occurrenceCount))
-        expect_output(print(search_guids(c("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59"),occurrence_count=TRUE)),"occurrenceCount")        
+        expect_output(print(search_guids(c("urn:lsid:biodiversity.org.au:afd.name:292120"),occurrence_count=TRUE)),"occurrenceCount")        
         expect_output(print(search_guids(c("urn:lsid:biodiversity.org.au:afd.taxon:95773568","isdulfsadh"),occurrence_count=TRUE)),"occurrenceCount")
-        expect_null(search_guids(c("urn:lsid:biodiversity.org.au:afd.taxon:95773568-053d-44de-a624-5699f0ac4a59","isdulfsadh"),occurrence_count=FALSE)$occurrenceCount)
+        expect_null(search_guids(c("urn:lsid:biodiversity.org.au:afd.name:292120","isdulfsadh"),occurrence_count=FALSE)$occurrenceCount)
         expect_equal(length(grep("occurrenceCount",capture.output(print(search_guids(c("urn:lsid:biodiversity.org.au:afd.taxon:95773568","isdulfsadh"),occurrence_count=FALSE))))),0) ## "occurrenceCount" should not appear in the print(...) output
     })
 }
