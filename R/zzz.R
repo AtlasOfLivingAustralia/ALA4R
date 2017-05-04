dfeabae691d659e333381861ce24cdc74a59244c
Ben Raymond <ben@theraymonds.org>
2017-05-04 21:56:25 +1000

    update onload message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-    packageStartupMessage("*NOTE, April 2017* --- some ALA4R functions may not work correctly due to back-end server changes. This includes the occurrences() function, and full-text searching [particularly specieslist(taxon=...)]. The issue is being investigated.")
+    packageStartupMessage("*NOTE, May 2017* --- some field (column) names in returned data objects may have changed with recent changes to the ALA infrastructure. These are being investigated.")
 }
