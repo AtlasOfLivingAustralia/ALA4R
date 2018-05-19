5e9bc966cd72cda60e458af79e77941c629e74b8
Ben Raymond <ben@theraymonds.org>
2018-05-19 19:42:04 +1000

    minor code reformat

--- R/image_info.R
+++ R/image_info.R
@@ -6,7 +6,7 @@
 #' @param image_number character or numeric: ID numbers of images (e.g. as returned by ALA's image search at \url{http://images.ala.org.au/}. Each image_number will be of a format something like 122218480
 #' @param verbose logical: show additional progress information? [default is set by ala_config()]
 #' @return A data.frame with one row per \code{id}, and at least the columns imageIdentifier and imageURL
-#' @seealso \code{\link{ala_config}} \code{\link{occurrences}}
+#' @seealso \code{\link{ala_config}}, \code{\link{occurrences}}
 #' @examples
 #' \dontrun{
 #' image_info(c("84654e14-dc35-4486-9e7c-40eb2f8d3faa",
@@ -15,7 +15,7 @@
 #' }
 #' @export image_info
 
-image_info <- function(id,image_number,verbose=ala_config()$verbose) {
+image_info <- function(id, image_number, verbose=ala_config()$verbose) {
     if (missing(id) & missing(image_number)) stop("image id or number must be provided")
     if (!missing(id)) assert_that(is.character(id))
     if (!missing(image_number)) assert_that(is.character(image_number) || is.numeric(image_number))
@@ -34,7 +34,7 @@ image_info <- function(id,image_number,verbose=ala_config()$verbose) {
     ## we get a 500 error if we ask for a non-existent image ID, so catch these errors with the on_server_error parm
     pages <- vapply(this_url, function(z) paste0(cached_get(URLencode(z), type="text", verbose=verbose, on_server_error=function(z)NULL), collapse=" "), FUN.VALUE="", USE.NAMES=FALSE)
     ## keep only the table from each, which has the actual image details
-    pages <- vapply(pages, function(z) { if (nrow(str_locate(z,"<table"))==1) str_extract(z,regex("<table.*</table>",dotall=TRUE)) else stop("image information page in unexpected format. ", getOption("ALA4R_server_config")$notify) }, FUN.VALUE="", USE.NAMES=FALSE)        
+    pages <- vapply(pages, function(z) { if (nrow(str_locate(z, "<table"))==1) str_extract(z, regex("<table.*</table>", dotall=TRUE)) else stop("image information page in unexpected format. ", getOption("ALA4R_server_config")$notify) }, FUN.VALUE="", USE.NAMES=FALSE)        
     temp <- extract_image_detail(pages, ".*?")
     ## this only has rows where the imageIdentifier had a valid match
     ## reconstruct a data.frame with the same row ordering as the input ids
