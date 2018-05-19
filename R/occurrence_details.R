2a5cf57962c790d9763d48126ab27e740f47488c
Ben Raymond <ben@theraymonds.org>
2018-05-19 14:46:19 +1000

    use vapply not sapply, code formatting

--- R/occurrence_details.R
+++ R/occurrence_details.R
@@ -11,11 +11,11 @@
 #' @examples
 #' \dontrun{
 #' s1 <- occurrence_details("f259c5ce-200c-41a2-b73a-e36a91f748f7")
-#' str(s1,max.level=3)
+#' str(s1, max.level=3)
 #' }
 #' @export occurrence_details
 
-occurrence_details <- function(uuid,verbose=ala_config()$verbose) {
+occurrence_details <- function(uuid, verbose=ala_config()$verbose) {
     if (!missing(uuid)) {
         assert_that(is.character(uuid))
     } else {
@@ -23,16 +23,9 @@ occurrence_details <- function(uuid,verbose=ala_config()$verbose) {
     }
     assert_that(is.flag(verbose))
     non_empty <- nchar(uuid)>0 & !is.na(uuid)
-    this_url <- sapply(uuid[non_empty],function(z)build_url_from_parts(getOption("ALA4R_server_config")$base_url_biocache,paste0("occurrence/",z)))
-    out_non_empty <- lapply(this_url,function(z)cached_get(z,type="json",verbose=verbose))
-    out <- vector("list",length(uuid))
-    #if (is.null(out)) {
-    #    ## invalid guids will give NULL here, catch them now
-    #    if (ala_config()$warn_on_empty) {
-    #        warning("no results found")
-    #    }
-    #    return(list())
-                                        #}
+    this_url <- vapply(uuid[non_empty], function(z) build_url_from_parts(getOption("ALA4R_server_config")$base_url_biocache, paste0("occurrence/", z)), FUN.VALUE="", USE.NAMES=FALSE)
+    out_non_empty <- lapply(this_url, function(z) cached_get(z, type="json", verbose=verbose))
+    out <- vector("list", length(uuid))
     non_empty_which <- which(non_empty)
     for (k in 1:length(uuid)) {
         if (non_empty[k]) {
@@ -43,11 +36,11 @@ occurrence_details <- function(uuid,verbose=ala_config()$verbose) {
                 ## some renaming of variables
                 if (!is.null(out[[k]]$processed$classification)) {
                     try({
-                        tempcols<-setdiff(names(out[[k]]$processed$classification),unwanted_columns(type="general"))
-                        out[[k]]$processed$classification<-out[[k]]$processed$classification[tempcols]
-                        names(out[[k]]$processed$classification)<-str_replace_all(names(out[[k]]$processed$classification),"^classs","class")
-                        names(out[[k]]$processed$classification)<-rename_variables(names(out[[k]]$processed$classification),type="general")
-                    },silent=TRUE)
+                        tempcols<-setdiff(names(out[[k]]$processed$classification), unwanted_columns(type="general"))
+                        out[[k]]$processed$classification <- out[[k]]$processed$classification[tempcols]
+                        names(out[[k]]$processed$classification) <- str_replace_all(names(out[[k]]$processed$classification), "^classs", "class")
+                        names(out[[k]]$processed$classification) <- rename_variables(names(out[[k]]$processed$classification), type="general")
+                    }, silent=TRUE)
                 }
             }
         } else {
