e13a20b56ed99b9ec6ea04dc17c2a8a5b7f3ce95
Ben Raymond <ben@theraymonds.org>
2018-05-19 15:22:01 +1000

    doc update, code reformat

--- R/ala_lists.R
+++ R/ala_lists.R
@@ -1,5 +1,7 @@
 #' Species lists
 #'
+#' Retrieves a specified species list. See \code{\link{ala_lists}} to find all available species lists.
+#'
 #' Note that this refers to pre-generated lists of species stored on the ALA servers. The similarly-named but different function \code{\link{specieslist}} provides a different function, namely listing the species matching a query or recorded as present in a search area.
 #'
 #' @references \url{http://lists.ala.org.au} and the associated web services at \url{http://lists.ala.org.au/ws}
@@ -9,36 +11,39 @@
 #'
 #' @return data.frame
 #'
-#' @seealso \code{\link{specieslist}} \code{\link{ala_lists}}
+#' @seealso \code{\link{specieslist}}, \code{\link{ala_lists}}
 #'
 #' @examples
 #' \dontrun{
 #'  all_lists <- ala_lists()
 #'  ## find the "Field Guide apps species profiles" from Museum Victoria
-#'  all_lists[grep("Field Guide",all_lists$listName),]
+#'  all_lists[grep("Field Guide", all_lists$listName), ]
+#' 
 #'  ## download the vertebrates one
 #'  l <- ala_list(druid="dr1146")
 #' }
 #'
 #' @export
-ala_list <- function(druid,kvp=TRUE,verbose=ala_config()$verbose) {
+ala_list <- function(druid, kvp=TRUE, verbose=ala_config()$verbose) {
     assert_that(is.string(druid))
     assert_that(is.flag(kvp))
     assert_that(is.flag(verbose))
     valid_ids <- ala_lists()$dataResourceUid
-    if (!druid %in% valid_ids) stop(sprintf("the supplied data resource UID (%s) does not appear to be a valid list ID",druid))
+    if (!druid %in% valid_ids) stop(sprintf("the supplied data resource UID (%s) does not appear to be a valid list ID", druid))
     if (kvp) {
-        this_url <- build_url_from_parts(getOption("ALA4R_server_config")$base_url_lists,c("speciesListItems",druid),list(includeKVP="true"))
+        this_url <- build_url_from_parts(getOption("ALA4R_server_config")$base_url_lists, c("speciesListItems", druid), list(includeKVP="true"))
     } else {
-        this_url <- build_url_from_parts(getOption("ALA4R_server_config")$base_url_lists,c("speciesListItems",druid))
+        this_url <- build_url_from_parts(getOption("ALA4R_server_config")$base_url_lists, c("speciesListItems", druid))
     }
-    cached_get(this_url,type="json",verbose=verbose)
+    cached_get(this_url, type="json", verbose=verbose)
 }
 
 
 
 
 #' Find ALA species lists
+#'
+#' Returns all available species lists. See \code{\link{ala_list}} to retrieve a specified list.
 #'
 #' @references \url{http://lists.ala.org.au} and the associated web services at \url{http://lists.ala.org.au/ws}
 #' @param guid string: (optional) if provided, return only lists in which this GUID appears
@@ -58,15 +63,15 @@ ala_list <- function(druid,kvp=TRUE,verbose=ala_config()$verbose) {
 #' }
 #'
 #' @export
-ala_lists <- function(guid,offset=0,max=500,verbose=ala_config()$verbose) {
+ala_lists <- function(guid, offset=0, max=500, verbose=ala_config()$verbose) {
     assert_that(is.flag(verbose))
     if (!missing(guid)) {
         assert_that(is.string(guid))
-        this_url <- build_url_from_parts(getOption("ALA4R_server_config")$base_url_lists,c("species",guid),list(offset=offset,max=max))
-        cached_get(this_url,type="json",verbose=verbose)
+        this_url <- build_url_from_parts(getOption("ALA4R_server_config")$base_url_lists, c("species", guid), list(offset=offset, max=max))
+        cached_get(this_url, type="json", verbose=verbose)
     } else {
-        this_url <- build_url_from_parts(getOption("ALA4R_server_config")$base_url_lists,"speciesList",list(offset=offset,max=max))
-        cached_get(this_url,type="json",verbose=verbose)$lists
+        this_url <- build_url_from_parts(getOption("ALA4R_server_config")$base_url_lists, "speciesList", list(offset=offset, max=max))
+        cached_get(this_url, type="json", verbose=verbose)$lists
     }
 }
 
