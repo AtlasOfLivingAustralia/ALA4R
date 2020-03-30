235aa987fc29494f7ea14f59eab41111d6e223ef
matilda2812 <matildastevenson2812@gmail.com>
2020-03-31 09:08:55 +1100

    Fix assertion tests

--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -1,31 +1,40 @@
 context("Check assertion-related functions")
 
-thischeck=function() {
+thischeck <- function() {
     test_that("NULL returned when no assertions present in input", {
-        temp=data.frame()
-        class(temp)=c("occurrences",class(temp))
+        temp <- data.frame()
+        class(temp) <- c("occurrences",class(temp))
         expect_null(check_assertions(temp))
     })
 }
 check_caching(thischeck)
 
 
-thischeck=function() {
+thischeck <- function() {
     test_that("check_assertions checks class of input correctly", {
-        temp=data.frame()
+        temp <- data.frame()
         expect_error(check_assertions(temp))    
-        class(temp)=c("occurrences",class(temp))
+        class(temp) <- c("occurrences",class(temp))
         expect_null(check_assertions(temp))
     })
 }
 check_caching(thischeck)
 
-thischeck=function() {
+thischeck <- function() {
     test_that("check_assertions gets all assertions in occurrences object", {
         skip_on_cran()
-        x=occurrences(taxon="Amblyornis newtonianus",download_reason_id=10,qa=ala_fields("assertions",as_is=TRUE)$name)    
-        expect_equal(length(setdiff(check_assertions(x)$name,ala_fields("assertions",as_is=TRUE)$name)),0) ## expect all assertion fields in object to be in the list of master assertion fields 
-        x=occurrences(taxon="Amblyornis newtonianus",download_reason_id=10,qa="none")
+        x <- occurrences(taxon="Amblyornis newtonianus",
+                         email="testing@test.org",
+                         download_reason_id=10,
+                         qa=ala_fields("assertions",as_is=TRUE)$name)  
+        ## expect all assertion fields in object to be in the list of master
+        ## assertion fields 
+        expect_equal(length(setdiff(check_assertions(x)$name,
+                                    ala_fields("assertions",
+                                               as_is=TRUE)$name)),0) 
+        x <- occurrences(taxon="Amblyornis newtonianus",
+                         email="testing@test.org",
+                         download_reason_id=10,qa="none")
         expect_null(check_assertions(x)$name)
     })
 }
