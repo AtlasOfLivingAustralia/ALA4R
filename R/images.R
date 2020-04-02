836c518b42ea27696aaaf7cca2f33a46722e62d1
matilda2812 <matildastevenson2812@gmail.com>
2020-04-02 13:00:14 +1100

    Fix method name change

--- R/images.R
+++ R/images.R
@@ -26,7 +26,7 @@
 #' }
 #' @export images
 #' @rdname image_info
-
+images(x) <- image_info(x)
 
 images <- function(id, download=FALSE, download_path,
                          verbose=ala_config()$verbose) {
