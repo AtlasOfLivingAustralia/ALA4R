5af3ff7c9e06fbe6744e37f90b0405f9834a43c3
matilda2812 <matildastevenson2812@gmail.com>
2020-06-29 11:14:15 +1000

    Handle reasons change in api

--- tests/testthat/test-occurrences.R
+++ tests/testthat/test-occurrences.R
@@ -5,8 +5,8 @@ thischeck <- function() {
     test_that("ala_reasons works as expected", {
         skip_on_cran()
         expect_named(ala_reasons(),c("rkey","name","id"))
-        expect_equal(nrow(ala_reasons()),13)
-        expect_equal(sort(ala_reasons()$id),c(0:8,10:13))
+        expect_equal(nrow(ala_reasons()),12)
+        expect_equal(sort(ala_reasons()$id),c(0:8,10:12))
         ## this should throw an error because there is an unused argument
         expect_error(ala_reasons(TRUE)) 
         tmp <- ala_reasons()
