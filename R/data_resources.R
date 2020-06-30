9c005b75e9560b6ec64f5be7814fd838cea1e3a6
matilda2812 <matildastevenson2812@gmail.com>
2020-06-30 16:51:30 +1000

    Add data resource search

---
+++ R/data_resources.R
@@ -0,0 +1,122 @@
+#' Get a list of current data resources
+#'
+#' Retrieve a list of all existing data resources, and basic information
+#' for each data resource. For data resource stats, use
+#' `data_resource_details.R`
+#'
+#' @references \itemize {
+#' \item Associated ALA web service for listing data resources:
+#' \url{https://collections.ala.org.au/ws/dataResource}
+#' }
+#' 
+#' @param druid string: data resource UID of the data resource(s)
+#' @param verbose logical: show additional progress information? 
+#' [default is set by ala_config()]
+#' @param max integer: (optional) if all data resources are requested, max 
+#' number to return, sorted by record count. Default is top 100 
+#'
+#' @return data frame of data resources
+#' @export data_resources
+
+data_resources <- function(druid, verbose=ala_config()$verbose, max=100) {
+  this_query <- list()
+  assert_that(is.flag(verbose))
+  
+  if (is.null(getOption("ALA4R_server_config")$base_url_collectory)) {
+    base_url <- 'https://collections.ala.org.au/ws/'
+  }
+  else {
+    base_url <- getOption("ALA4R_server_config")$base_url_collectory    
+  }
+  
+  if(missing(druid)) {
+    this_url <- paste0(getOption("ALA4R_server_config")$base_url_biocache,
+                       "occurrence/facets?facets=data_resource_uid&flimit=",
+                       max)
+    drs <- cached_get(URLencode(this_url), type="json", verbose=verbose,
+                      caching = "off")
+    druid <- vapply(drs$fieldResult[[1]]$i18nCode, function(x) {
+      sub(".*\\.", "", x)
+      }, "")
+  }
+  
+  assert_that(is.character(druid))
+  
+  dr_data <- do.call(rbind, lapply(druid, function(z) {
+    cols <- c("uid", "name", "licenseType", "dateCreated","lastUpdated",
+              "doi","Animalia","Bacteria", "Plantae","Chromista","Fungi",
+              "Protista","Protozoa","Virus","Unknown",
+              "totalDownloadedRecords","resourceType", "gbifRegistryKey")
+    
+    this_url <- paste0(base_url, "dataResource/", z)
+    data <- cached_get(URLencode(this_url), type="json", verbose=verbose,
+                       on_server_error = function(z){NULL})
+    if (is.null(data)) {
+      data <- as.data.frame(list(uid = z, totalRecords = 0))
+    }
+    else {
+      data <- data[names(data) %in% cols]
+      data[vapply(data, is.null, logical(1))] <- NA
+      
+      data$totalRecords <- occurrences(fq=paste0 ('data_resource_uid:',z),
+                                  record_count_only = TRUE)
+    }
+    
+    df <- as.data.frame(data)
+    
+    if (df$totalRecords>0) {
+      # add lifeform counts
+      df <- cbind(df, lifeform_stats(z, verbose=verbose))
+      
+      # add download stats
+      df$totalDownloadedRecords <- download_stats(z, verbose=verbose)
+    }
+    else {
+      # add missing cols so rows are of equal length
+      df[cols[!(cols %in% colnames(df))]] = NA
+    }
+    
+    return(df)
+    }))
+  row.names(dr_data) <- NULL
+  # Warn if any data resources were invalid
+  if(NA %in% unique(dr_data$name)) {
+    warning("One or more of the data resources requested is invalid or
+            has been deleted")
+  }
+  dr_data
+}
+
+
+download_stats <- function(id,verbose=ala_config()$verbose) {
+  temp_logger_url <- "https://logger.ala.org.au/service/"
+  this_url <- paste0(temp_logger_url,
+                     "reasonBreakdown?eventId=1002&entityUid=", id)
+  data <- cached_get(URLencode(this_url), type="json", verbose=verbose)
+  return(data$all$records)
+  
+}
+
+# Return lifeform stats for data resource. 
+# It should also be possible to break down by other ranks in future 
+lifeform_stats <- function(id, rank = "lifeform",verbose=ala_config()$verbose) {
+  this_url <- paste0(getOption("ALA4R_server_config")$base_url_biocache,
+                     "breakdown/dataResources/",id,"?rank=",rank)
+  
+  data <- cached_get(URLencode(this_url), type="json", verbose=verbose)
+ 
+  kingdoms <- c("Animalia","Bacteria", "Plantae","Chromista",
+                   "Fungi","Protista","Protozoa","Virus","Unknown")
+  
+  # label count with no kingdom
+  data$taxa$label <- sub("^$", "Unknown", data$taxa$label)
+  counts <- data$taxa$count
+  names(counts) <- data$taxa$label
+  
+  
+  # add 0 counts for kingdoms not present
+  zero_counts <- rep(0,length(kingdoms[!(kingdoms %in% names(counts))]))
+  names(zero_counts) <- kingdoms[!(kingdoms %in% names(counts))]
+  
+  return(as.list(append(counts,zero_counts)))
+}
---
+++ tests/testthat/test-data_resources.R
@@ -0,0 +1,47 @@
+context("Test data resource information retrieval functions")
+
+cols <- c("uid", "name", "licenseType", "dateCreated","lastUpdated","doi",
+          "Animalia","Bacteria", "Plantae","Chromista","Fungi","Protista",
+          "Protozoa","Virus","Unknown","totalDownloadedRecords","totalRecords",
+          "resourceType", "gbifRegistryKey")
+
+thischeck <- function() {
+  test_that("data resources returns correct data columns", {
+    skip_on_cran()
+    expect_equal(sort(cols),sort(names(data_resources('dr375'))))
+  })
+}
+
+check_caching(thischeck)
+
+
+thischeck <- function() {
+  test_that("data resources returns a row for every id provided", {
+    skip_on_cran()
+    result <- data_resources(c('dr375','dr8128','dr743'))
+    expect_equal(nrow(result),3)
+  })
+}
+
+check_caching(thischeck)
+
+
+
+thischeck <- function() {
+  test_that("data resources behaves correctly with a single invalid id", {
+    skip_on_cran()
+    expect_warning(data_resources('invalid-id'))
+  })
+}
+
+check_caching(thischeck)
+
+thischeck <- function() {
+  test_that("data resources behaves correctly with one valid and one invalid id", {
+    skip_on_cran()
+    expect_warning(data_resources(c('invalid-id', 'dr345')))
+  })
+}
+
+check_caching(thischeck)
+
