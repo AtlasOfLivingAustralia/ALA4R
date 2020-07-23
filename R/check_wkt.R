7a778ddc5cf27a9fb10fa679d2a552bcecedea4d
matilda2812 <matildastevenson2812@gmail.com>
2020-07-24 08:35:08 +1000

    Update docs

--- R/check_wkt.R
+++ R/check_wkt.R
@@ -1,7 +1,8 @@
 ## simple function to check that a given WKT string is valid
-## prior to v1.25 this was an ALA4R-specific (and not very comprehensive) function
+## prior to v1.25 this was an ALA4R-specific (and not very comprehensive)
+## function
 ## as of v1.25 we rely on wellknown::lint
 check_wkt <- function(wkt) {
-    assert_that(is.string(wkt)) ## leave this in place for consistency with older code
-    wellknown::lint(wkt)
+  assert_that(is.string(wkt)) ## leave for consistency with older code
+  wellknown::lint(wkt)
 }
