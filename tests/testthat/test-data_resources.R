872516958f9fae4c6ab64bd9e71b1730e0be2724
matilda2812 <matildastevenson2812@gmail.com>
2020-07-23 11:58:15 +1000

    Fix style

--- tests/testthat/test-data_resources.R
+++ tests/testthat/test-data_resources.R
@@ -1,13 +1,13 @@
 context("Test data resource information retrieval functions")
 
-cols <- c("uid", "name", "licenseType", "dateCreated","lastUpdated","doi",
-          "totalDownloadedRecords","totalRecords",
+cols <- c("uid", "name", "licenseType", "dateCreated", "lastUpdated", "doi",
+          "totalDownloadedRecords", "totalRecords",
           "resourceType", "gbifRegistryKey")
 
 thischeck <- function() {
   test_that("data resources returns correct data columns", {
     skip_on_cran()
-    expect_true(all(cols %in% names(data_resources('dr375'))))
+    expect_true(all(cols %in% names(data_resources("dr375"))))
   })
 }
 
@@ -17,10 +17,10 @@ check_caching(thischeck)
 thischeck <- function() {
   test_that("data resources returns a row for every id provided", {
     skip_on_cran()
-    result <- data_resources(c('dr375','dr8128','dr743'))
-    expect_equal(nrow(result),3)
+    result <- data_resources(c("dr375", "dr8128", "dr743"))
+    expect_equal(nrow(result), 3)
     result <- data_resources(max = 3)
-    expect_equal(nrow(result),3)
+    expect_equal(nrow(result), 3)
   })
 }
 
@@ -31,16 +31,17 @@ check_caching(thischeck)
 thischeck <- function() {
   test_that("data resources behaves correctly with a single invalid id", {
     skip_on_cran()
-    expect_warning(data_resources('invalid-id'))
+    expect_warning(data_resources("invalid-id"))
   })
 }
 
 check_caching(thischeck)
 
 thischeck <- function() {
-  test_that("data resources behaves correctly with one valid and one invalid id", {
+  test_that("data resources behaves correctly with one valid and one invalid
+            id", {
     skip_on_cran()
-    expect_warning(data_resources(c('invalid-id', 'dr345')))
+    expect_warning(data_resources(c("invalid-id", "dr345")))
   })
 }
 
