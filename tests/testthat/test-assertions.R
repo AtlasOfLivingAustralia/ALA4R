d6b7e34d1012545c9e3b78aefa26e9131979a8e8
matilda2812 <matildastevenson2812@gmail.com>
2020-08-26 15:18:38 +1000

    Unskip tests

--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -23,19 +23,19 @@ check_caching(thischeck)
 thischeck <- function() {
     test_that("check_assertions gets all assertions in occurrences object", {
         skip_on_cran()
-        #x <- occurrences(taxon = "Amblyornis newtonianus",
-        #                 email = "testing@test.org",
-        #                 download_reason_id = 10,
-         #                qa = ala_fields("assertions", as_is = TRUE)$name)
+        x <- occurrences(taxon = "Amblyornis newtonianus",
+                         email = "testing@test.org",
+                         download_reason_id = 10,
+                         qa = ala_fields("assertions", as_is = TRUE)$name)
         ## expect all assertion fields in object to be in the list of master
         ## assertion fields
-        #expect_equal(length(setdiff(check_assertions(x)$name,
-        #                            ala_fields("assertions",
-        #                                       as_is = TRUE)$name)), 0)
-        #x <- occurrences(taxon = "Amblyornis newtonianus",
-        #                 email = "testing@test.org",
-        #                 download_reason_id = 10, qa = "none")
-        #expect_null(check_assertions(x)$name)
+        expect_equal(length(setdiff(check_assertions(x)$name,
+                                    ala_fields("assertions",
+                                               as_is = TRUE)$name)), 0)
+        x <- occurrences(taxon = "Amblyornis newtonianus",
+                         email = "testing@test.org",
+                         download_reason_id = 10, qa = "none")
+        expect_null(check_assertions(x)$name)
     })
 }
 check_caching(thischeck)
