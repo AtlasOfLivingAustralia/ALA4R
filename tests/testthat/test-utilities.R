a9f5127780fe3ffd54facb7f0e9c6af1b7d9f5c3
matilda2812 <matildastevenson2812@gmail.com>
2020-10-02 11:17:43 +1000

    Update config test

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -90,7 +90,7 @@ test_that("ala config works as expected", {
   conf <- ala_config()
   expect_equal(c(conf$download_reason_id, conf$warn_on_empty, conf$caching,
                  conf$verbose, conf$text_encoding),
-               c(NA, FALSE, "on", FALSE, "UTF-8"))
+               c(4, FALSE, "on", FALSE, "UTF-8"))
   expect_error(ala_config(bad_option = 3))
   # test numeric conversion
   ala_config(download_reason_id = "testing")
