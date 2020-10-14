ee68c539b599a3d70c4c3b781d9906dc62c2cda2
matilda2812 <matildastevenson2812@gmail.com>
2020-10-14 13:12:39 +1100

    Combine image downloads into ala_media and add tests

--- R/ala_media.R
+++ R/ala_media.R
@@ -2,9 +2,93 @@
 #' 
 #' @param identifier string: a single or vector of identifiers, of type
 #' specified by `identifier_type`
-#' @param identifier_type string: one of `c("occurrence", "image")`
+#' @param download_dir string: path to directory for downloading media
+#' @param identifier_type string: one of `c("occurrence", "media")`
+#' @param media_type string: type of media to download, one or both of 
+#' `c("image", "sound")`
 
-
-ala_media <- function(identifier, identifier_type) {
+# should sounds be downloaded by default?
+# which fields should be kept?
+ala_media <- function(identifier, download_dir, identifier_type = "media",
+                      media_type = c("image", "sound")) {
+  assert_that(!missing(identifier),
+              msg = "Please provide at least one identifier")
+  assert_that(is.character(identifier))
   
+  valid_media_type <- str_detect(media_type, c("image", "sound"))
+  if (!all(valid_media_type)) {
+    stop("Valid media types are `c('image', 'sound')`")
+  }
+  assert_that(identifier_type %in% c("occurrence", "media"),
+              msg = "`identifier_type` must be one of `c('occurrence', 'media')`")
+
+  
+  assert_that(!missing(download_dir), msg = "Directory to download media to is
+              required")
+  assert_that(file.exists(download_dir))
+  
+  media_data <- data.table::rbindlist(lapply(identifier, function(id) {
+    if (identifier_type == 'media') {
+      # get the media specified
+      data <- cbind(mediaId = id, media(id))
+    } else {
+      # get media for an occurrence id
+      data <- occurrence_media(id)
+    }
+    if (!is.null(data)) {
+      adjust_col_names(data, type = "media")
+    }
+  }), fill = TRUE)
+  
+  # download media
+  # should add output path to out data?
+  d <- mapply(download_media, media_data$mediaId, media_data$format,
+              download_dir)
+  media_data
+}
+
+# should also allow filters? 
+# handle the case when an occurrence record has more images than the limit?
+# is there a limit?
+occurrence_media <- function(occurrence_id) {
+  url <- parse_url(getOption("ALA4R_server_config")$base_url_images)
+  url$path <- c("ws", "search")
+  url$query <- list(q = paste0("occurrenceID:", "\"", occurrence_id, "\""))
+  resp <- fromJSON(build_url(url))
+  if(resp$totalImageCount == 0) {
+    warning("No media was found for id ", "\"", occurrence_id, "\"")
+    return(NULL)
+  }
+  adjust_col_names(resp$images, type = "media")
+}
+
+# what to do if no media found? create error for now
+media <- function(media_id) {
+  url <- parse_url(getOption("ALA4R_server_config")$base_url_images)
+  url$path <- c("ws", "image", media_id)
+  tryCatch(
+    resp <- fromJSON(build_url(url), flatten = TRUE),
+    error = function(e) {
+      e$message <- paste0("No media found for id ", "\"", media_id, "\"")
+      stop(e)
+    }
+  )
+  # workaround for recognised licence returning null
+  if (is.null(resp$recognisedLicence)) {
+    resp$recognisedLicence <- NA
+  }
+  adjust_col_names(as.data.frame(resp, stringsAsFactors = FALSE),
+                   type = "media")
+}
+
+download_media <- function(id, type, download_dir) {
+  url <- parse_url(getOption("ALA4R_server_config")$base_url_images)
+  url$path <- c("image", as.character(id), "original")
+  ext <- switch (type,
+    'image/jpeg' = '.jpg'
+  )
+  out_path <- file.path(download_dir, paste0(id, ext))
+  download.file(build_url(url), destfile = out_path)
+  # throttle download requests
+  Sys.sleep(1)
 }
---
+++ tests/testthat/test-ala_media.R
@@ -0,0 +1,66 @@
+context("Test ALA media")
+
+test_that("ala media check inputs", {
+  image_id <- "24fbddc4-a95e-4e97-b588-e76321c9cc73"
+  expect_error(ala_media())
+  expect_error(ala_media(identifier = image_id,
+                         identifier_type = "bad_type"))
+  expect_error(ala_media(identifier = 1))
+  expect_error(ala_media(image_id,
+               download_dir = "bad_path"))
+  expect_error(ala_media(image_id))
+  
+  
+})
+
+test_that("ala media handles a single image id", {
+  skip_on_cran()
+  media_dir <- "test_media"
+  dir.create(media_dir)
+  expect_error(ala_media(identifier = "bad_id", identifier_type = "media",
+                         download_dir = media_dir))
+  media_data <- ala_media(identifier = "24fbddc4-a95e-4e97-b588-e76321c9cc73",
+                          identifier_type = "media", download_dir = media_dir)
+  file_count <- length(list.files(media_dir))
+  expect_equal(file_count, nrow(media_data))
+  
+  unlink(media_dir)
+})
+
+test_that("ala media handles multiple image ids", {
+  skip_on_cran()
+  media_dir <- "test_media"
+  dir.create(media_dir)
+  media_data <- ala_media(identifier = c("24fbddc4-a95e-4e97-b588-e76321c9cc73",
+                                         "61237df1-1939-44d3-b63a-c09213f9a261"),
+                          identifier_type = "media", download_dir = media_dir)
+  file_count <- length(list.files(media_dir))
+  expect_equal(file_count, nrow(media_data))
+  unlink(media_dir)
+})
+
+test_that("ala media handles a single occurrence id", {
+  skip_on_cran()
+  media_dir <- "test_media"
+  dir.create(media_dir)
+  media_data <- ala_media(identifier = "80b1d0d6-2ca5-475a-a014-302f05d51839",
+                          identifier_type = "occurrence",
+                          download_dir = media_dir)
+  file_count <- length(list.files(media_dir))
+  expect_equal(file_count, nrow(media_data))
+  unlink(media_dir)
+  
+})
+
+test_that("ala media handles a multiple occurrence ids", {
+  skip_on_cran()
+  media_dir <- "test_media"
+  dir.create(media_dir)
+  media_data <- ala_media(identifier = c("80b1d0d6-2ca5-475a-a014-302f05d51839",
+                                         "0053e37a-4bf7-4f6d-be2d-e66645b668c6"),
+                          identifier_type = "occurrence", download_dir = media_dir)
+  file_count <- length(list.files(media_dir))
+  expect_equal(file_count, nrow(media_data))
+  unlink(media_dir)
+  
+})
