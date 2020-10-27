e661288bb6484715f6a07b87c7cf588d00677221
matilda2812 <matildastevenson2812@gmail.com>
2020-10-27 15:52:23 +1100

    Extract post function

---
+++ R/ala_POST.R
@@ -0,0 +1,16 @@
+# Wrapper for getting data
+# 
+# Try using crul
+ala_POST <- function(url, path, body = list()) {
+  cli <- HttpClient$new(
+    url = url,
+    headers = list(
+      useragent = ala_config()$user_agent
+    )
+  )
+  res <- cli$post(path = path, body = body, encode = "json")
+  if (res$status_code != 200) {
+    stop("Status code ", res$status_code, "returned for url ", res$request$url)
+  }
+  res$parse("UTF-8")
+}
