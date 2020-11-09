543f18880a6f13d42ae0f380226b9525bf3b3e38
matilda2812 <matildastevenson2812@gmail.com>
2020-11-09 11:40:29 +1100

    Add more ala_counts tests

--- tests/testthat/test-ala_counts.R
+++ tests/testthat/test-ala_counts.R
@@ -15,8 +15,27 @@ test_that("ala counts checks inputs", {
 test_that("ala counts returns expected outputs", {
   expect_type(ala_counts(taxon_id = "https://id.biodiversity.org.au/taxon/apni/51302291"), "integer")
   expect_equal(class(ala_counts(taxon_id = "https://id.biodiversity.org.au/taxon/apni/51302291",
-                          breakdown = "basis_of_record")), "data.frame")
+                                breakdown = "basis_of_record")), "data.frame")
   
   expect_warning(ala_counts(breakdown = 'phylum'))
 })
 
+test_that("ala counts works with filters", {
+  skip_on_cran()
+  expect_lt(ala_counts(filters = ala_filters(list(year = 2000))),
+            ala_counts())
+})
+
+test_that("ala_counts handles wkt area inputs", {
+  # invalid wkt
+  skip_on_cran()
+  wkt <- readLines('../testdata/short_act_wkt.txt')
+  expect_lt(ala_counts(area = wkt), ala_counts())
+})
+
+test_that("ala counts handles queries with no records", {
+  skip_on_cran()
+  filters <- ala_filters(list(kingdom = 'non-existent'))
+  expect_s3_class(ala_counts(filters = filters,
+                             breakdown = 'basis_of_record'), "data.frame")
+})
