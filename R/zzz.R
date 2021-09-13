475289b6860339ab272f73eab4ab96853ff799de
Martin Westgate <martinjwestgate@gmail.com>
2021-09-13 10:40:59 +1000

    Make clearer load message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  msg <- "`ALA4R` is deprecated and will be removed from CRAN at the end of 2021. The package to replace `ALA4R``,
-`galah`, is now available (https://CRAN.R-project.org/package=galah). For an introduction to `galah`,
-visit the GitHub page (https://github.com/AtlasOfLivingAustralia/galah)."
+  msg <- "`ALA4R` is deprecated; we suggest you use `galah`instead (https://CRAN.R-project.org/package=galah). 
+    For an introduction to `galah`, visit the GitHub page (https://github.com/AtlasOfLivingAustralia/galah)."
   packageStartupMessage(msg)
 }
